692451fbe9428602deee8a98d943557ce218f8fc
David Huard <david.huard@gmail.com>
2018-09-22 19:02:19 -0400

    gr4j smoke test

--- tests/test_LHMP_models.py
+++ tests/test_LHMP_models.py
@@ -8,8 +8,10 @@ def test_gr4j():
     time = pd.date_range('2000-01-01', freq='D', periods=365*3)
     n = len(time)
     temp = pd.Series(-20*np.sin(np.arange(n)/365.25 * 2 * np.pi)+5, index=time)
-    precip = pd.Series(((np.random(n)-.5)*10).clip(0), index=time)
-    evap = np.clip(temp/200 + np.random(n)*10, 0)
+    precip = pd.Series(((np.random.rand(n)-.5)*20).clip(0), index=time)
+    evap = (temp/400 + (np.random.rand(n)-.5)*8).clip(0)
     df = pd.DataFrame({'Temp':temp, 'Evap':evap, 'Prec':precip})
 
-    return df
+    params = [300, -1, 200, 2, .5, 8]
+    out = models.gr4j(df, params)
+    return out
