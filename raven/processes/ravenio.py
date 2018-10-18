956e67e14d75e6bbda3ed19162673732e2cb8374
David Huard <david.huard@gmail.com>
2018-10-18 15:52:26 -0400

    function to create subdir with files and exec

--- raven/processes/ravenio.py
+++ raven/processes/ravenio.py
@@ -1,3 +1,5 @@
+import os
+
 def rv_format(fn, kind='i', **kwds):
     """Read the model input template file and fill the given arguments."""
 
@@ -11,3 +13,21 @@ def rv_format(fn, kind='i', **kwds):
         txt = f.read()
 
     return txt.format(**kwds)
+
+
+def create_subdir(name, outpath, **kwds):
+    """Create a subdirectory and write the RAVEN configuration files. """
+    from raven.models import raven_templates
+    inpath = raven_templates[name]
+
+    # Create subdirectory
+    os.mkdir(os.path.join(outpath, 'model'))
+    os.mkdir(os.path.join(outpath, 'output'))
+
+    for kind in 'chipt':
+        txt = rv_format(name, kind, **kwds)
+        with open(os.path.join(outpath, model, name + '.rv' + kind), 'w') as f:
+            f.write(txt)
+
+    os.symlink(os.path.join(..., 'raven_rev.exe'))
+
