81a83542281622bb5f43c838db5a7b81df7209f1
David Huard <david.huard@gmail.com>
2018-10-22 16:20:51 -0400

    first working version of the raven gr4j process

--- tests/test_ravenio.py
+++ tests/test_ravenio.py
@@ -9,7 +9,6 @@ import tempfile
 from collections import OrderedDict as odict
 import glob
 
-
 class TestStartDate:
 
     def test_grj4_cemaneige(self):
@@ -24,11 +23,14 @@ class TestStartDate:
 
 class TestSetupModel:
     gr4j = odict(
-        rvi=dict(Start_Date='2000-01-01 00:00:00', End_Date=None, Duration=10, TimeStep=1.0, EvaluationMetrics='NASH_SUTCLIFFE RMSE'),
+        rvi=dict(run_name='run1', Start_Date='2000-01-01 00:00:00', End_Date=None, Duration=10, TimeStep=1.0, EvaluationMetrics='NASH_SUTCLIFFE RMSE'),
         rvp=odict(GR4J_X1=.7, GR4J_X2=.7, GR4J_X3=19., GR4J_X4=2.09, AvgAnnualSnow=123, AirSnowCoeff=.75),
         rvc=odict(SOIL_0=1, SOIL_1=2),
         rvh=dict(NAME='Test', AREA=45, ELEVATION=3, LATITUDE=45, LONGITUDE=-154),
-        rvt=dict(RAIN='data/data.nc', SNOW='data/data.nc', TMIN='data/data.nc', TMAX='data/data.nc', PET='data/data.nc', QOBS='data/data.nc')
+        rvt=dict(pr='data/data.nc', prsn='data/data.nc', tasmin='data/data.nc', tasmax='data/data.nc',
+                 evspsbl='data/data.nc', water_volume_transport_in_river_channel='data/data.nc',
+                 pr_var='rain', prsn_var='snow', tasmin_var='tmin', tasmax_var='tmax', evspsbl_var='pet',
+                 water_volume_transport_in_river_channel_var='qobs')
     )
 
     def test_gr4j(self):
@@ -42,5 +44,19 @@ class TestSetupModel:
                 txt = f.read()
                 assert '{' not in txt
                 assert '}' not in txt
+
+@pytest.mark.skip
+class TestReadDiagnostics:
+    def test_simple(self):
+        import StringIO
+        diag = StringIO.StringIO()
+        diag.write('observed data series,filename,DIAG_NASH_SUTCLIFFE,DIAG_RMSE,\n'
+                   'HYDROGRAPH,data_obs/Salmon-River-Near-Prince-George_meteo_daily.nc,-0.746096,62.1502,')
+        diag.seek(0)
+
+        out = ravenio.read_diagnostics(diag)
+        assert out['DIAG_RMSE'] == 62.1502
+
+
 
 
