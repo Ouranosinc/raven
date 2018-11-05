90d231ca0dabcc71fcc49e533ce543b451d80d49
David Huard <david.huard@gmail.com>
2018-11-05 14:11:37 -0500

    added test for hmets run with rvt time series

--- tests/test_wps_raven.py
+++ tests/test_wps_raven.py
@@ -9,16 +9,15 @@ from . common import client_for, TESTDATA, CFG_FILE
 from raven.processes import RavenProcess
 
 cf = ['rvi', 'rvp', 'rvc', 'rvh', 'rvt']
-pattern = TESTDATA['raven-gr4j-cemaneige-nc-rv']
-rvfiles = {os.path.splitext(fn)[1][1:]: os.path.join(os.path.split(pattern)[0], fn) for fn in glob.glob(pattern)}
-for f in rvfiles.values():
-    if not os.path.exists(f):
-        raise ValueError("{} does not exist".format(f))
 
 class TestRavenProcess:
 
     def test_gr4j_saumon_nc(self):
         client = client_for(Service(processes=[RavenProcess(), ], cfgfiles=CFG_FILE))
+
+        pattern = TESTDATA['raven-gr4j-cemaneige-nc-rv']
+        rvfiles = {os.path.splitext(fn)[1][1:]: os.path.join(os.path.split(pattern)[0], fn) for fn in
+                   glob.glob(pattern)}
 
         datainputs = ("nc=files@xlink:href=file://{fn};" + \
                      ';'.join(["conf=files@xlink:href=file://{%s}"%key for key
@@ -28,9 +27,28 @@ class TestRavenProcess:
         resp = client.get(
             service='WPS', request='Execute', version='1.0.0', identifier='raven',
             datainputs=datainputs)
-        print(resp.response[0])
         assert_response_success(resp)
 
+
+    def test_hmets(self):
+        client = client_for(Service(processes=[RavenProcess(), ], cfgfiles=CFG_FILE))
+
+        pattern = TESTDATA['raven-hmets-rv']
+        rvfiles = {os.path.splitext(fn)[1][1:]: os.path.join(os.path.split(pattern)[0], fn) for fn in
+                   glob.glob(pattern)}
+
+        pattern = TESTDATA['raven-hmets-ts']
+        nc = [os.path.join(os.path.split(pattern)[0], fn) for fn in glob.glob(pattern)]
+
+        datainputs = ("nc=files@xlink:href=file://{};nc=files@xlink:href=file://{};" + \
+                      ';'.join(["conf=files@xlink:href=file://{%s}" % key for key
+                                in cf])) \
+            .format(*nc, **rvfiles)
+
+        resp = client.get(
+            service='WPS', request='Execute', version='1.0.0', identifier='raven',
+            datainputs=datainputs)
+        assert_response_success(resp)
 
 
 "rvi=files@xlink:href=file://{rvi};" \
