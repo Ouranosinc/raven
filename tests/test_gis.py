ced1144c62d1a1457eaf4d956db1cc0ee7f922cb
Trevor James Smith <trevor_smith@live.com>
2018-11-08 11:07:11 -0500

    Added rasterio as requirement and added raster CreateCopy methods

--- tests/test_gis.py
+++ tests/test_gis.py
@@ -13,8 +13,11 @@ class TestGISClasses:
 
         rvn = RavenShape(shape)
         assert rvn.crs() == '4326'
+        assert not rvn.check_crs(crs=666)
+        assert rvn.check_crs(raster=dem)
         assert len(rvn) == 137
         assert rvn.clip(dem)
+
 
     def test_raven_raster(self):
         here = os.getcwd()
@@ -28,21 +31,22 @@ class TestGISClasses:
         assert rvn.check_crs(shape=shape)
 
 
-# if __name__ == "__main__":
-#     here = os.getcwd()
-#
-#     dem = os.path.join(here, '../tests/testdata/earthenv_dem_90m/earthenv_dem90_southernQuebec.tiff')
-#     shape = os.path.join(here, '../tests/testdata/donneesqc_mrc_poly/donnees_quebec_mrc_polygones.gml')
-#
-#     rvn = RavenShape(shape)
-#
-#     print(rvn)
-#
-#     for centroid in rvn.centroids():
-#         print(centroid)
-#
-#     for clip in rvn.clip(dem):
-#         print(clip)
-#
-#     for region in rvn.average(raster=dem):
-#         print(region)
+
+if __name__ == "__main__":
+    here = os.getcwd()
+
+    dem = os.path.join(here, '../tests/testdata/earthenv_dem_90m/earthenv_dem90_southernQuebec.tiff')
+    shape = os.path.join(here, '../tests/testdata/donneesqc_mrc_poly/donnees_quebec_mrc_polygones.gml')
+
+    rvn = RavenShape(shape)
+
+    print(rvn)
+
+    for centroid in rvn.centroids():
+        print(centroid)
+
+    for clip in rvn.clip(dem):
+        print(clip)
+
+    for region in rvn.average(raster=dem):
+        print(region)
