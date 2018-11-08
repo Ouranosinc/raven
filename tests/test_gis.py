4952e26fc74f4e09bed7175edb5246d9da69bb98
Trevor James Smith <trevor_smith@live.com>
2018-11-08 17:05:52 -0500

    Removed rasterio, added warp/reprojection, pixel counts, and more shape methods

--- tests/test_gis.py
+++ tests/test_gis.py
@@ -18,7 +18,6 @@ class TestGISClasses:
         assert len(rvn) == 137
         assert rvn.clip(dem)
 
-
     def test_raven_raster(self):
         here = os.getcwd()
 
@@ -31,22 +30,36 @@ class TestGISClasses:
         assert rvn.check_crs(shape=shape)
 
 
-
-if __name__ == "__main__":
-    here = os.getcwd()
-
-    dem = os.path.join(here, '../tests/testdata/earthenv_dem_90m/earthenv_dem90_southernQuebec.tiff')
-    shape = os.path.join(here, '../tests/testdata/donneesqc_mrc_poly/donnees_quebec_mrc_polygones.gml')
-
-    rvn = RavenShape(shape)
-
-    print(rvn)
-
-    for centroid in rvn.centroids():
-        print(centroid)
-
-    for clip in rvn.clip(dem):
-        print(clip)
-
-    for region in rvn.average(raster=dem):
-        print(region)
+# if __name__ == "__main__":
+#     here = os.getcwd()
+#
+#     dem = os.path.join(here, '../tests/testdata/earthenv_dem_90m/earthenv_dem90_southernQuebec.tiff')
+#     shape = os.path.join(here, '../tests/testdata/donneesqc_mrc_poly/donnees_quebec_mrc_polygones.gml')
+#
+#     rvn = RavenShape(shape)
+#
+#     print(rvn)
+#
+#     print(rvn.layer_bounds())
+#
+#     for parcel in rvn.feature_bounds():
+#         print(parcel)
+#
+#     for parcel in rvn.area(crs=3857):
+#         print(parcel)
+#
+#     for centroid in rvn.centroids():
+#         print(centroid)
+#
+#     for clip in rvn.clip(dem):
+#         print(clip)
+#
+#     for region in rvn.average(raster=dem):
+#         print(region)
+#
+#     # rvn = RavenRaster(dem)
+#
+#     rvn.warp(crs=4269)
+#     print(rvn.projected())
+#
+#     print(rvn.pixel_counts())
