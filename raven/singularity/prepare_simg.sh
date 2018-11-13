aaaafea9ef4dfd1418ba6ca3bb4fb6f76accc396
Marc Lalonde <marc.lalonde@crim.ca>
2018-11-13 11:16:47 -0500

    Add singularity recipe for Raven app

---
+++ raven/singularity/prepare_simg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+singularity create ubuntu.img
+sudo singularity bootstrap ubuntu.img ubuntu.def
+# Expecting already logged to the singularity registry
+export SREGISTRY_CLIENT=registry
+sregistry push --name hydro/raven --tag latest ubuntu.img
+
+
