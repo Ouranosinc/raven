020804c9ca4ca358af35cbb9cc6eedf104a645fe
Zeitsperre <trevor_smith@live.com>
2018-11-26 12:45:39 -0500

    Disabled broken model and redundant before_script

--- raven/processes/__init__.py
+++ raven/processes/__init__.py
@@ -2,12 +2,12 @@ from .wps_wordcounter import WordCounter
 from .wps_inout import InOut
 from .wps_sleep import Sleep
 from .wps_gr4j_cemaneige import GR4JCemaNeigeProcess
-from .wps_raven_gr4j_cemaneige import RavenGR4JCemaNeigeProcess
+# from .wps_raven_gr4j_cemaneige import RavenGR4JCemaNeigeProcess
 from .wps_raven_hmets import RavenHMETSProcess
 
 processes = [
     GR4JCemaNeigeProcess(),
-    RavenGR4JCemaNeigeProcess(),
+    # RavenGR4JCemaNeigeProcess(),
     WordCounter(),
     InOut(),
     Sleep(),
