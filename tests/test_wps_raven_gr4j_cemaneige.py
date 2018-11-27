7ec6674b20f835e513806619802b75f09970d095
Juliane Mai <juliane.mai@uwaterloo.ca>
2018-11-26 23:11:05 -0500

    Including tests for Raven's GR4J and HMETS test
    The tests are confirmed to yield the same results as reported in the Wiki using the full time period (20819  days starting 1954-01-01). But for the tests they run on a shorter period (only 2 years) due to runtime.

--- tests/test_wps_raven_gr4j_cemaneige.py
+++ tests/test_wps_raven_gr4j_cemaneige.py
@@ -1,5 +1,6 @@
 import pytest
 import datetime as dt
+import numpy as np
 
 from pywps import Service
 from pywps.tests import assert_response_success
@@ -50,4 +51,16 @@ class TestRavenGR4JCemaNeigeProcess:
         out = get_output(resp.xml)
         assert 'diagnostics' in out
         tmp_file, _ = urlretrieve(out['diagnostics'])
-        assert 'DIAG' in open(tmp_file).read()
+        tmp_content = open(tmp_file).readlines()
+
+        # checking correctness of NSE (full period 1954-2011 would be NSE=-0.738921)
+        assert 'DIAG_NASH_SUTCLIFFE' in tmp_content[0]
+        idx_diag = tmp_content[0].split(',').index("DIAG_NASH_SUTCLIFFE")
+        diag = np.float(tmp_content[1].split(',')[idx_diag])
+        np.testing.assert_almost_equal(diag, -0.0905, 4, err_msg='NSE is not matching expected value')
+
+        # checking correctness of RMSE (full period 1954-2011 would be RMSE=62.0224)
+        assert 'DIAG_RMSE' in tmp_content[0]
+        idx_diag = tmp_content[0].split(',').index("DIAG_RMSE")
+        diag = np.float(tmp_content[1].split(',')[idx_diag])
+        np.testing.assert_almost_equal(diag, 37.5124, 4, err_msg='RMSE is not matching expected value')
