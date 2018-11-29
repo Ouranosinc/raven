c6c765114474b7addeb19885d881f767bce7c382
David Huard <david.huard@gmail.com>
2018-11-29 08:32:49 -0500

    removed demo processes. fixed gr4j test failure

--- raven/processes/__init__.py
+++ raven/processes/__init__.py
@@ -1,6 +1,3 @@
-from .wps_wordcounter import WordCounter
-from .wps_inout import InOut
-from .wps_sleep import Sleep
 from .wps_raven import RavenProcess
 from .wps_gr4j_cemaneige import GR4JCemaNeigeProcess
 from .wps_raven_gr4j_cemaneige import RavenGR4JCemaNeigeProcess
@@ -11,7 +8,4 @@ processes = [
     GR4JCemaNeigeProcess(),
     RavenGR4JCemaNeigeProcess(),
     RavenHMETSProcess(),
-    WordCounter(),
-    InOut(),
-    Sleep(),
 ]
