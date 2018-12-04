15d011e47d295bf5605df726aa6a8f805cda9e3e
Trevor James Smith <trevor_smith@live.com>
2018-12-04 10:32:36 -0500

    Added Python3.7 to supported versions

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ classifiers = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Topic :: Scientific/Engineering :: Atmospheric Science',
     'License :: OSI Approved :: MIT License',
 ]
