9ce8769e7ac340b506e4c37bc7a2e4b18b96516f
David Huard <david.huard@gmail.com>
2018-12-07 17:20:15 -0500

    all tests now pass

--- tests/test_rv.py
+++ tests/test_rv.py
@@ -1,12 +1,12 @@
 import pytest
-from raven.models import RV
+from raven.models.rv import RV, RVI
 import datetime as dt
 from collections import namedtuple
 
 class TestRV:
 
     def test_end_date(self):
-        rvi = RV(run_name='test',
+        rvi = RVI(run_name='test',
                   start_date=dt.datetime(2000, 1, 1),
                   end_date=dt.datetime(2000, 1, 11),
                   )
@@ -17,12 +17,15 @@ class TestRV:
         assert dt.datetime(2000, 1, 12) == rvi.end_date
 
     def test_params(self):
-        class MyClass(RV):
+        class RVP(RV):
             params = namedtuple('p', 'x, y')
 
-        rv = MyClass()
-        rv['params'] = MyClass.params(1, 2)
-        assert rv.params.x == 1
+        rvp = RVP()
+        rvp.params = RVP.params(1, 2)
+        assert rvp.params.x == 1
+
+        d = rvp.to_dict()
+        assert 'x' in d
 
     def test_dict_interface(self):
         rv = RV(run_name='test')
@@ -32,17 +35,9 @@ class TestRV:
         with pytest.raises(AttributeError):
             rv['r'] = 6
 
-    def test_flatten(self):
-        class MyClass(RV):
-            params = namedtuple('p', 'x, y')
-
-        rv = MyClass(params=MyClass.params(1, 2))
-        d = rv.flatten()
-        assert 'x' in d
-
     def test_evaluation_metrics(self):
-        rv = RV()
-        rv.evaluation_metrics = 'LOG_NASH'
+        rvi = RVI()
+        rvi.evaluation_metrics = 'LOG_NASH'
 
         with pytest.raises(ValueError):
-            rv.evaluation_metrics = 'JIM'
+            rvi.evaluation_metrics = 'JIM'
