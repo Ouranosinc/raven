92eabaedffa746b3a5584ea6d3bcea3cd8b87eda
Zeitsperre <trevor_smith@live.com>
2018-12-10 16:54:06 -0500

    Added support for CRS transformations and cleaned input calls.

--- raven/processes/wps_shape_area.py
+++ raven/processes/wps_shape_area.py
@@ -1,13 +1,21 @@
 import logging
-
 import re
+from functools import partial
+
 import fiona
-from shapely.geometry import shape
+import shapely.ops as ops
+from fiona.crs import from_epsg, from_string
+from pyproj import Proj, transform
 from pywps import LiteralInput, ComplexInput
 from pywps import LiteralOutput
-from pywps import Process
+from pywps import Process, FORMATS
+from shapely.geometry import shape
 
 LOGGER = logging.getLogger("PYWPS")
+
+LAEA = '+proj=laea +lat_0=90 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs'
+WORLDMOLL = '+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs'
+ALBERS = '+proj=aea +lat_1=0 +lat_2=0 +lon_0=0 +ellps=WGS84 +x_0=0 +y_0=0 +units=m +no_defs'
 
 
 class ShapeArea(Process):
@@ -15,30 +23,13 @@ class ShapeArea(Process):
 
     def __init__(self):
         inputs = [
-            LiteralInput('expand features', 'Examine all features in shapefile', data_type='boolean'),
+            LiteralInput('use_all_features', 'Examine all features in shapefile', data_type='boolean', default='false'),
+            LiteralInput('crs', 'EPSG Coordinate Reference System code', data_type='int', default='4326'),
             ComplexInput('shape', 'Vector Shape',
-                         abstract='An URL pointing to either an ESRI Shapefile, GML, GeoJSON, or any other file in a'
-                                  ' standard vector format. The ESRI Shapefile must be zipped and contain the .shp,'
-                                  ' .shx, and .dbf. The shape CRS definition should also match the DEM CRS.',
-                         min_occurs=1,
-                         default=None, supported_formats=[])]
+                         abstract='An URL pointing to either an ESRI Shapefile, GML, JSON, GeoJSON.'
+                                  ' The ESRI Shapefile must be zipped and contain the .shp, .shx, and .dbf.',
+                         min_occurs=1, supported_formats=[FORMATS.GML, FORMATS.GEOJSON, FORMATS.SHP, FORMATS.JSON])]
         # supported_formats=[
-        #            # shp
-        #            {mimeType: 'application/zip',
-        #            encoding: '???',
-        #            schema: None},
-        #
-        #            # gml
-        #            {mimeType: 'text/xml',
-        #            encoding:'utf-8',
-        #            schema:'http://schemas.opengis.net/gml/3.2.1/gml.xsd'},
-        #
-        #            # json
-        #            {mimeType: 'text/plain',
-        #            encoding: 'iso-8859-2',
-        #            schema: None
-        #            },
-        #
         #            # kml
         #            {mimeType: 'text/xml',
         #            encoding: 'windows-1250',
@@ -46,7 +37,7 @@ class ShapeArea(Process):
         #            ])
 
         outputs = [
-            LiteralOutput('area', 'Area Calculations', data_type='float', abstract='Area of shape in sq. km.'),
+            LiteralOutput('area', 'Area Calculations', data_type='float', abstract='Area of shape in m^2'),
             LiteralOutput('centroids', 'Centroid Locations', data_type='float',
                           abstract="Geographic locations of feature centroids"),
             LiteralOutput('schemas', 'Feature schemas',
@@ -85,34 +76,42 @@ class ShapeArea(Process):
 
         shape_fn = request.inputs['shape'][0].file
         shape_url = address_append(shape_fn)
-
+        shape_crs = request.inputs['crs'][0]
+        features = request.inputs['use_all_features'][0]
         schemas = []
         centroids = []
         areas = []
 
-        features = False
-        if request.inputs['expand features'][0]:
-            features = True
-
         for i, layername in enumerate(fiona.listlayers(shape_url)):
             if features:
-                with fiona.open(shape_url, 'r', layer=i) as src:
+                with fiona.open(shape_url, 'r', crs=from_epsg(shape_crs), layer=i) as src:
                     geom = shape(src['geometry'])
 
                     schemas.append(src.schema)
                     centroids.append(geom.centroid)
-                    areas.append(geom.area)
+                    transformed = ops.transform(
+                        partial(
+                            transform,
+                            Proj(src.crs),
+                            Proj(from_string(LAEA))),
+                        geom)
+                    areas.append(transformed.area)
 
-                    src.close()
             else:
-                with fiona.open(shape_url, 'r') as src:
+                with fiona.open(shape_url, 'r', crs=from_epsg(shape_crs)) as src:
                     geom = shape(src['geometry'])
 
                     schemas.append(src.schema)
                     centroids.append(geom.centroid)
-                    areas.append(geom.area)
+                    transformed = ops.transform(
+                        partial(
+                            transform,
+                            Proj(src.crs),
+                            Proj(from_string(LAEA))),
+                        geom)
+                    areas.append(transformed.area)
 
-                    src.close()
+            src.close()
 
         response.outputs['area'].data = areas
         response.outputs['centroids'].data = centroids
