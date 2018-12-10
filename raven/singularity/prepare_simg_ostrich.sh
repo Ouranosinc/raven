0d6edaad73787a91cb84f1c58675cd8b5780be5a
Marc Lalonde <marc.lalonde@crim.ca>
2018-12-09 22:19:33 -0500

    Replace default image names with meaningful names

--- raven/singularity/prepare_simg_ostrich.sh
+++ raven/singularity/prepare_simg_ostrich.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-\rm -f ubuntu.img
-singularity create ubuntu.img
-sudo singularity bootstrap ubuntu.img ubuntu_ostrich.def
+\rm -f ostrich.img
+singularity create ostrich.img
+sudo singularity bootstrap ostrich.img ubuntu_ostrich.def
 # Expecting already logged to the singularity registry
 export SREGISTRY_CLIENT=registry
-sregistry push --name hydro/ostrich --tag latest ubuntu.img
+sregistry push --name hydro/ostrich --tag latest ostrich.img
 
 
