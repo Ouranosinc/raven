1abc7d623b44033cf1baaef0bbf9c8d16e75b277
Zeitsperre <trevor_smith@live.com>
2018-12-13 12:00:32 -0500

    Early work on ZonalStatsProcess

--- raven/processes/wps_zonal_stats.py
+++ raven/processes/wps_zonal_stats.py
@@ -1,10 +1,13 @@
 import logging
+import os
 
 from pywps import LiteralInput, ComplexInput
-from pywps import LiteralOutput
+from pywps import LiteralOutput, ComplexOutput
 from pywps import Process, FORMATS
 from pywps.app.Common import Metadata
 from rasterstats import zonal_stats
+
+from raven.utils import extract_archive
 
 LOGGER = logging.getLogger("PYWPS")
 
@@ -30,12 +33,6 @@ class ZonalStatisticsProcess(Process):
                                   ' standard vector format. The ESRI Shapefile must be zipped and contain the .shp,'
                                   ' .shx, and .dbf. The shape CRS definition should also match the DEM CRS.',
                          min_occurs=1, supported_formats=[FORMATS.GEOJSON, FORMATS.GML, FORMATS.JSON, FORMATS.SHP]),
-            # supported_formats=[
-            #            # kml
-            #            {mimeType: 'text/xml',
-            #            encoding: 'windows-1250',
-            #            schema: 'http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd'}
-            #            ])
             ComplexInput('raster', 'Gridded Raster Data set',
                          abstract='An URL pointing at the DEM to be queried. Defaults to the USGS HydroSHEDS DEM.',
                          # TODO: Include details (resolution, version).
@@ -55,13 +52,12 @@ class ZonalStatisticsProcess(Process):
             LiteralOutput('sum', 'Sum of pixels', data_type='integer', abstract='Sum of all pixel values'),
             LiteralOutput('nodata', 'Number of null data pixels', data_type='integer',
                           abstract='Number of null data pixels'),
-            # TODO: Figure out a replacement for dictionary or find a way of supporting dictionaries with WPS
-            # LiteralOutput('categories', 'Counts of pixels by category', data_type='dict',
-            #               abstract='Counts of pixels by category'),
+            ComplexOutput('categories', 'Counts of pixels by category',
+                          abstract='Counts of pixels by category'),
         ]
 
         super(ZonalStatisticsProcess, self).__init__(
-            self._rasterstats_handler,
+            self._zonalstats_handler,
             identifier="rasterstats",
             title="Raster Zonal Statistics",
             version="1.0",
@@ -73,30 +69,72 @@ class ZonalStatisticsProcess(Process):
             store_supported=True)
 
     @staticmethod
-    def _rasterstats_handler(request, response):
+    def _zonalstats_handler(request, response):
 
-        dem_fn = request.inputs['raster'][0].file
-        shape_fn = request.inputs['shape'][0].file
-        band = request.inputs['band'][0]
-        touches = request.inputs['select_all_touching'][0]
-        categorical = request.inputs['categorical'][0]
+        # dem_fn = request.inputs['raster'][0].file
+        # shape_fn = request.inputs['shape'][0].file
+        # band = request.inputs['band'][0]
+        # touches = request.inputs['select_all_touching'][0]
+        # categorical = request.inputs['categorical'][0]
 
-        # TODO: Figure out whether lists are accepted outputs
+        dem_url = request['raster']
+        shape_url = request['shape']
+        band = request['band']
+        touches = request['select_all_touching']
+        categorical = request['categorical']
+
+        archive_types = ['.nc', '.tar', '.zip']
+        allowed_types = ['.gml', '.shp', '.geojson', '.json', '.gpkg']
+
+        # TODO: I know this is ugly. Suggestions welcome.
+        if any(ext in shape_url for ext in archive_types):
+            extracted = extract_archive(shape_url, os.getcwd())  # self.workdir)
+            for potential_vector in extracted:
+                if any(ext in potential_vector for ext in allowed_types):
+                    shape_url = potential_vector
+
+        dem_files = any(dem in extract_archive(dem_url) for dem in ['.tiff', '.tif'])
+
+        print(dem_files)
+
         try:
             if not categorical:
-                stats = zonal_stats(shape_fn, dem_fn, all_touched=touches, band=band,
+                stats = zonal_stats(shape_url, dem_url, all_touched=touches, band=band,
                                     stats=['count', 'min', 'max', 'mean', 'median', 'sum', 'nodata'])
                 for key in stats.keys():
-                    response.outputs[key].data = stats[key]
-
+                    # response.outputs[key].data = stats[key]
+                    response[key] = stats[key]
             else:
                 stats = zonal_stats(
-                    shape_fn, dem_fn, band=band, categorical=categorical, all_touched=touches, geojson_out=True)
+                    shape_url, dem_url, band=band, categorical=categorical, all_touched=touches, geojson_out=True)
                 # response.outputs['categories'].data = stats[1]
-                pass
+                response['categories'] = stats[1]
 
         except Exception as e:
             msg = 'Failed to perform zonal statistics: {}'.format(e)
             raise Exception(msg)
 
         return response
+
+
+if __name__ == "__main__":
+    from tests.common import TESTDATA
+
+    fields = ['select_all_touching={select_all_touching}', 'categorical={categorical}', 'band={band}',
+              'crs={crs}', 'shape=file@xlink:href=file://{shape}', 'raster=file@xlink:href=file://{raster}']
+
+    inputs = {'select_all_touching': True,
+              'categorical': False,
+              'band': 1,
+              'crs': 4326,
+              'shape': TESTDATA['watershed_vector'],
+              'raster': TESTDATA['hydrosheds_conditioned']}
+
+    datainputs = ';'.join(fields).format(**inputs)
+
+    response = {'count': None, 'min': None, 'max': None, 'mean': None, 'median': None, 'sum': None, 'nodata': None}
+
+    q = ZonalStatisticsProcess._zonalstats_handler(request=inputs, response=response)
+
+    for k in q.keys():
+        print(k, q[k])
