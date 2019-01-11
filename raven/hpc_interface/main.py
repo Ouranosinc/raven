3672c98b3eb87e45e7a981294c812242c31da1df
Marc Lalonde <marc.lalonde@crim.ca>
2019-01-11 10:29:41 -0500

    Refactor code

--- raven/hpc_interface/main.py
+++ raven/hpc_interface/main.py
@@ -1,8 +1,9 @@
 from random import sample
 from string import digits, ascii_uppercase, ascii_lowercase
-from os import path
-#from fabric import Connection
+
 from pssh.clients import ParallelSSHClient
+from pssh.exceptions import AuthenticationException, UnknownHostException, ConnectionErrorException
+
 import pprint
 import sys
 import getopt
@@ -10,16 +11,13 @@ import time
 import os
 import shutil
 import tempfile
-import os.path
-hostname = "cedar.computecanada.ca"
-user = "lalondem"
-home_dir = path.join("/home",user)
-src_data_path = "./data/"
-do_cleanup = True
-template_dir = None
-dataset = None
-batch_cmd_template = "batch_template.txt"
-output_folder=""
+
+#src_data_path = "./data/"
+#do_cleanup = True
+#template_dir = None
+#dataset = None
+#batch_cmd_template = "batch_template.txt"
+#output_folder=""
 from subprocess import run
 from gevent import joinall
 
@@ -30,144 +28,268 @@ def rand_fname(length=8):
 
     return fname
 
-def copy_data_to_remote(c, datapath, dataset_, remote_path, remote_temp_folder):
+class HPCConnection(object):
 
-    c.run_command("mkdir "+path.join(remote_path, remote_temp_folder))
+    def __init__(self, external_init_dict=None):
 
-#    data_path_content = os.listdir(path=src_data_path)
-#    assert(len(data_path_content) == 1)
-#    df_basename = data_path_content[0]
-    df_basename = dataset_
-    remote_tar = path.join(remote_path, remote_temp_folder, "data.tar")
-    print("system cmd: "+"tar cvf /tmp/"+remote_temp_folder+".tar -C "+ path.join(datapath,df_basename)+" .")
-    os.system("tar cvf /tmp/"+remote_temp_folder+".tar -C "+ path.join(datapath,df_basename)+" .")
-    g=c.scp_send("/tmp/"+remote_temp_folder+".tar", remote_tar)
-    joinall(g, raise_error=True)
-    destpath = path.join(remote_path, remote_temp_folder)
-    print(destpath)
-    output = c.run_command("tar xvf "+remote_tar+" -C "+destpath)
-    errmsg = next(output[hostname]["stderr"], None)
-    if errmsg is not None:
-        print("Error: " + errmsg)
-    errmsg = next(output[hostname]["stdout"], None)
-    if errmsg is not None:
-        print("stdout: " + errmsg)
+        init_dict = {}
+        if external_init_dict is not None:
+            init_dict = external_init_dict
 
-    os.remove("/tmp/"+remote_temp_folder+".tar")
-    #print(next(output[hostname]["stdout"]))
-    return df_basename
+        self.hostname = init_dict.get("hostname", "cedar.computecanada.ca")
+        self.user = init_dict.get("user", "lalondem")
+        self.home_dir = init_dict.get("home_dir", os.path.join("/home",self.user))
+        self.src_data_path = init_dict.get("src_data_path", "./data")
+        self.template_path = init_dict.get("template_path", "./")
+        self.batchscript_tmplt_filename = init_dict.get("batchscript_fname", "./batch_template.txt")
+
+        self.client = ParallelSSHClient([self.hostname], user=self.user)
+        self.remote_abs_working_folder = None
+        self.remote_working_folder = None
+        self.active_dataset_name = None
+        self.live_job_id = None
+
+
+    def check_connection(self):
+
+        status = True
+        msg = None
+
+        try:
+            self.client.run_command("ls")
+        except (AuthenticationException, UnknownHostException, ConnectionErrorException) as e:
+            status = False
+            msg = str(e)
+
+        return status, msg
+
+    def copy_data_to_remote(self, dataset_, remote_temp_folder=None):
+        """
+        Copies data contained in a local directory over to a remote destination
+        """
+
+        remote_base_path = self.home_dir
+        local_datapath = self.src_data_path
+        if remote_temp_folder is None:
+            remote_temp_folder = rand_fname()
+
+        full_remote_path = os.path.join(remote_base_path, remote_temp_folder)
+        remote_tar = os.path.join(full_remote_path, "data.tar")
+        self.remote_abs_working_folder = full_remote_path
+        self.remote_working_folder = remote_temp_folder
+        self.active_dataset_name = dataset_
+        self.client.run_command("mkdir "+full_remote_path)
+
+    #    data_path_content = os.listdir(path=src_data_path)
+    #    assert(len(data_path_content) == 1)
+    #    df_basename = data_path_content[0]
+        df_basename = dataset_
+
+        print("system cmd: "+"tar cvf /tmp/"+remote_temp_folder+".tar -C "+ os.path.join(local_datapath,df_basename)+" .")
+        os.system("tar cvf /tmp/"+remote_temp_folder+".tar -C "+ os.path.join(local_datapath,df_basename)+" .")
+        try:
+            g = self.client.scp_send("/tmp/"+remote_temp_folder+".tar", remote_tar)
+            joinall(g, raise_error=True)
+        except:
+            raise Exception("scp_send failed")
+
+        output = self.client.run_command("tar xvf "+remote_tar+" -C "+full_remote_path)
+        errmsg = next(output[self.hostname]["stderr"], None)
+        if errmsg is not None:
+            print("Error: " + errmsg)
+            raise Exception("Error untarring data file: "+errmsg)
+
+        errmsg = next(output[self.hostname]["stdout"], None)
+        if errmsg is not None:
+            print("stdout: " + errmsg)
+
+        os.remove("/tmp/"+remote_temp_folder+".tar")
+
 
 # output files in base_dir/jobname/out
-def copy_data_from_remote(c, base_dir, jobname, jobid, absolute_local_out_dir):
+    def copy_data_from_remote(self, jobid, absolute_local_out_dir, cleanup_temp=True):
 
-    absolute_tar_fname = path.join(base_dir, jobname, jobname+"_out.tar")
-    absolute_output_data_path = path.join(base_dir, jobname,"out")
-    stdout_file = path.join(home_dir,"slurm-"+jobid+".out")
+        absolute_tar_fname = os.path.join(self.remote_abs_working_folder, self.remote_working_folder+"_out.tar")
+        absolute_output_data_path = os.path.join(self.remote_abs_working_folder, "out")
+        stdout_file = os.path.join(self.home_dir,"slurm-"+jobid+".out")
 
-    try:
-        print("copy_data_from_remote: copying stdout: cp "+stdout_file+" "+ absolute_output_data_path)
+        try:
+            print("copy_data_from_remote: copying stdout: cp "+stdout_file+" "+ absolute_output_data_path)
 
-        output = c.run_command("cp "+stdout_file+" "+ absolute_output_data_path)
-        print("copy_data_from_remote: running tar")
-        output = c.run_command("tar cf "+absolute_tar_fname+" -C "+absolute_output_data_path+" .")
-        print("copy_data_from_remote: recv")
-        #g = c.scp_recv(absolute_tar_fname, path.join(absolute_local_out_dir,jobname+"_out.tar"))
-        g = c.copy_remote_file(absolute_tar_fname, "/tmp/"+jobname + "_out.tar")  #scp_recv
+            output = self.client.run_command("cp "+stdout_file+" "+ absolute_output_data_path)
+            print("copy_data_from_remote: running tar")
+            output = self.client.run_command("tar cf "+absolute_tar_fname+" -C "+absolute_output_data_path+" .")
+            print("copy_data_from_remote: recv")
+            local_tar = "/tmp/"+self.remote_working_folder + "_out.tar"
+            #g = c.scp_recv(absolute_tar_fname, path.join(absolute_local_out_dir,jobname+"_out.tar"))
+            g = self.client.copy_remote_file(absolute_tar_fname, local_tar)  #scp_recv
+            joinall(g, raise_error=True)
+            if not os.path.exists(absolute_local_out_dir):
+                #shutil.rmtree(absolute_local_out_dir)
+                os.mkdir(absolute_local_out_dir)
+
+            #os.mkdir(path.join(absolute_local_out_dir,jobname)
+            print("tar xf "+local_tar+"_"+self.hostname+" -C "+absolute_local_out_dir)
+            os.system("tar xf "+local_tar+"_"+self.hostname+" -C "+absolute_local_out_dir)
+            if cleanup_temp:
+                #os.remove(local_tar+"_" + self.hostname)
+                pass
+
+        except Exception as e:
+            print("No output from job: {}".format(e))
+        print("received")
+
+
+    # executable_ is either raven or ostrich
+    def copy_batchscript(self,executable_, datafile_basename, template_dir, batch_tmplt_fname, shub_hostname):
+
+
+        template_file = open(os.path.join(template_dir, batch_tmplt_fname), "r")
+        abs_remote_output_dir = os.path.join(self.remote_abs_working_folder, "out")
+        tmplt = template_file.read()
+        tmplt = tmplt.replace("ACCOUNT","def-fouchers")
+        tmplt = tmplt.replace("DURATION","00:20:00")
+        tmplt = tmplt.replace("TEMP_PATH", self.remote_abs_working_folder)
+        tmplt = tmplt.replace("INPUT_PATH", self.remote_abs_working_folder)
+        tmplt = tmplt.replace("OUTPUT_PATH", abs_remote_output_dir)
+        tmplt = tmplt.replace("DATAFILE_BASENAME", datafile_basename)
+        tmplt = tmplt.replace("SHUB_HOSTNAME", shub_hostname)
+        tmplt = tmplt.replace("EXEC", executable_)
+
+        #    subst_template_file, subst_fname = tempfile.mkstemp(suffix=".sh")
+        subst_fname = self.remote_working_folder+".sh"
+        file = open("/tmp/"+subst_fname, 'w')
+        file.write(tmplt)
+        file.close()
+
+        self.client.run_command("mkdir " + abs_remote_output_dir)
+        self.client.run_command("chmod 777 " + self.remote_abs_working_folder)
+        self.client.run_command("chmod 777 " + abs_remote_output_dir)
+        g = self.client.copy_file("/tmp/"+subst_fname, os.path.join(self.remote_abs_working_folder,subst_fname))
         joinall(g, raise_error=True)
-        if not os.path.exists(absolute_local_out_dir):
-            #shutil.rmtree(absolute_local_out_dir)
-            os.mkdir(absolute_local_out_dir)
+        self.client.run_command("chmod ugo+x " + os.path.join(self.remote_abs_working_folder,subst_fname))
+        os.remove("/tmp/"+subst_fname)
+
+        if executable_ == "ostrich":
+            # In addition, copy  raven script
+            r = os.path.join(self.remote_abs_working_folder,"Ost-RAVEN.sh")
+            srcfilee = os.path.join(self.template_path,"Ost-RAVEN.sh")
+            g=self.client.copy_file(srcfilee, r)
+            joinall(g, raise_error=True)
+            self.client.run_command("chmod ugo+x " + r)
+            self.client.run_command("mkdir " + self.remote_abs_working_folder + "/model/output")
+
+            self.client.run_command("chmod 777 " + self.remote_abs_working_folder + "/model/output")
 
 
-        #os.mkdir(path.join(absolute_local_out_dir,jobname)
-        #print("tar xf /media/sf_develop/pavics-hydro/2cc/"+jobname+"_out.tar_"+hostname+" -C "+absolute_local_out_dir)
-        os.system("tar xf /tmp/"+jobname+"_out.tar_"+hostname+" -C "+absolute_local_out_dir)
+        return os.path.join(self.remote_abs_working_folder,subst_fname)
 
-    except:
-        print("No output from job")
-    print("received")
+    def submit_job(self, script_fname):
 
+        output = self.client.run_command("sbatch --parsable " + script_fname)
+        errmsg = next(output[self.hostname]["stderr"], None)
+        if errmsg is not None:
+            raise Exception("Error: "+errmsg)
 
-# executable_ is either raven or ostrich
-def copy_batchscript(executable_, c, rootdir, rootfname, datafile_basename, batch_tmplt_fname):
+        self.live_job_id = next(output[self.hostname]["stdout"])
 
-    template_file = open(path.join(template_dir,batch_tmplt_fname), "r")
-    tmplt = template_file.read()
-    tmplt = tmplt.replace("ACCOUNT","def-fouchers")
-    tmplt = tmplt.replace("DURATION","00:20:00")
-    tmplt = tmplt.replace("TEMP_PATH", rootdir+rootfname)
-    tmplt = tmplt.replace("INPUT_PATH", rootdir + rootfname)
-    tmplt = tmplt.replace("OUTPUT_PATH", rootdir + rootfname+"/out")
-    tmplt = tmplt.replace("DATAFILE_BASENAME", datafile_basename)
-    tmplt = tmplt.replace("EXEC", executable_)
+        return self.live_job_id
 
-    #    subst_template_file, subst_fname = tempfile.mkstemp(suffix=".sh")
-    subst_fname = rootfname+".sh"
-    file = open("/tmp/"+subst_fname, 'w')
-    file.write(tmplt)
-    file.close()
+    def get_status(self, jobid):
 
-    c.run_command("mkdir " + rootdir + rootfname + "/out")
-    c.run_command("chmod 777 " + rootdir + rootfname)
-    c.run_command("chmod 777 " + rootdir + rootfname + "/out")
-    g = c.copy_file("/tmp/"+subst_fname,path.join(rootdir+rootfname,subst_fname))
-    joinall(g, raise_error=True)
-    c.run_command("chmod ugo+x "+path.join(rootdir+rootfname,subst_fname))
-    os.remove("/tmp/"+subst_fname)
+        cmd = "squeue -o '%T' -j {} --noheader".format(jobid)
 
-    if executable_ == "ostrich":
-        # In addition, copy  raven script
-        r = path.join(rootdir,rootfname,"Ost-RAVEN.sh")
-        srcfilee = path.join(template_dir,"Ost-RAVEN.sh")
-        g=c.copy_file(srcfilee, r)
-        joinall(g, raise_error=True)
-        c.run_command("chmod ugo+x " + r)
-        c.run_command("mkdir " + rootdir + rootfname + "/model/output")
+        output = self.client.run_command(cmd)
+        pp = pprint.PrettyPrinter(indent=4)
+    #    pp.pprint(output)
+        status_output = []  # 1 line expected
 
-        c.run_command("chmod 777 " + rootdir + rootfname + "/model/output")
+        errmsg = next(output[self.hostname]["stderr"], None)
+        if errmsg is not None:
+            if errmsg.find("Invalid job id specified"):
+                return "DONE"
+            print("stderr: "+errmsg)
+            raise Exception("Error: "+errmsg)
 
+        for line in output[self.hostname]["stdout"]:
+            #print("stdout: " + line)
+            status_output.append(line)
 
-    return path.join(rootdir+rootfname,subst_fname)
-
-def submit_job(client, script_fname):
-
-    output = client.run_command("sbatch --parsable " + script_fname)
-    errmsg = next(output[hostname]["stderr"], None)
-    if errmsg is not None:
-        raise Exception("Error: "+errmsg)
-
-    jobid = next(output[hostname]["stdout"])
-
-    return jobid
-
-def get_status(c, hostname, jobid):
-
-    cmd = "squeue -o '%T' -j {} --noheader".format(jobid)
-
-    output = c.run_command(cmd)
-    pp = pprint.PrettyPrinter(indent=4)
-#    pp.pprint(output)
-    status_output = []  # 1 line expected
-
-    errmsg = next(output[hostname]["stderr"], None)
-    if errmsg is not None:
-        if errmsg.find("Invalid job id specified"):
+        if len(status_output) == 0:
             return "DONE"
-        print("stderr: "+errmsg)
-        raise Exception("Error: "+errmsg)
+        elif len(status_output) >1:
+            print("too many outputs:")
+            pp.pprint(status_output)
+            raise Exception("Error: too many outputs")
 
-    for line in output[hostname]["stdout"]:
-        #print("stdout: " + line)
-        status_output.append(line)
+        return status_output[0]
 
-    if len(status_output) == 0:
-        return "DONE"
-    elif len(status_output) >1:
-        print("too many outputs:")
-        pp.pprint(status_output)
-        raise Exception("Error: too many outputs")
+    def cleanup(self, jobid):
 
-    return status_output[0]
+        output1 = self.client.run_command("rm -rf {}".format(self.remote_abs_working_folder))
+        output2 = self.client.run_command("rm slurm-{}.out".format(jobid))
+
+        try:
+            print(next(output1[self.hostname_]["stdout"]))
+            print(next(output2[self.hostname_]["stdout"]))
+            print(next(output1[self.hostname_]["stderr"]))
+            print(next(output2[self.hostname_]["stderr"]))
+        except:
+            pass
+
+
+class RavenHPCProcess(object):
+
+    def __init__(self, process_name, connection_cfg_dict=None):
+        """
+
+        :param process_name: 'raven' or 'ostrich'
+        :param connection_cfg_dict:
+        """
+
+        self.hpc_connection = HPCConnection(connection_cfg_dict)
+        self. process_name = process_name
+        self.live_job_id = None
+        self.shub_hostname = connection_cfg_dict.get("SHubHostname","132.217.141.54")
+
+    def check_connection(self):
+
+        status, msg = self.hpc_connection.check_connection()
+        if status == False:
+            return status, msg
+        response = os.system("ping -c 1 " + self.shub_hostname)
+        if response == 0:
+            return True, None
+        else:
+            return False, "shub server down"
+
+
+
+    def submit(self, dataset):
+
+        self.hpc_connection.copy_data_to_remote(dataset)
+
+        remote_abs_script_fname = self.hpc_connection.copy_batchscript(self.process_name, dataset, "./", "batch_template.txt", self.shub_hostname)
+        print("Running " + remote_abs_script_fname)
+        jobid = self.hpc_connection.submit_job(remote_abs_script_fname)
+        print("job id = " + jobid)
+        self.live_job_id = jobid
+
+    def retrieve(self, output_folder):
+
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+        self.hpc_connection.copy_data_from_remote(self.live_job_id, output_folder)
+
+    def monitor(self):
+
+        return self.hpc_connection.get_status(self.live_job_id)
+
+    def cleanup(self):
+
+        # remote
+        #self.hpc_connection.cleanup(self.live_job_id)
+        pass
 
 
 def process_cmd(executable_, client, hostname, option, jobname="", jobid=""):
@@ -223,6 +345,85 @@ src_data_dir/datasetname/datasetname.rv?
 
 
 
+
+
+
+def newmainfct(argv):
+    #        print("main.py command (Submit|Monitor|Retrieve)")
+
+    try:
+        opts, args = getopt.getopt(argv, "e:d:i:o:t:n")
+    except getopt.GetoptError:
+
+        print('main.py -e executable (raven|ostrich) -d workingdir -i dataset -o outputdir -t template_dir -n (no cleanup)')
+        sys.exit(2)
+
+    src_data_dir = "./"
+    out_dir = "./"
+    executable = None
+    for opt, arg in opts:
+        if opt == '-d':
+            src_data_dir = arg
+        elif opt == '-e':
+            if arg == "raven":
+                executable = "raven"
+            elif arg == "ostrich":
+                executable = "ostrich"
+        elif opt == "-i":
+            dataset = arg
+        elif opt == "-o":
+            out_dir = arg
+        elif opt == "-t":
+            template_dir = arg
+        elif opt == "-n":
+            do_cleanup = False
+
+    if dataset is None or template_dir is None:
+        print("Missing arg!")
+        exit(2)
+    if executable is None:
+        print("Missing executable!")
+        exit(2)
+
+#    src_data_path = src_data_dir
+
+    raven_process = RavenHPCProcess(executable,{"src_data_path":src_data_dir, "template_path":template_dir})
+
+    status, msg = raven_process.check_connection()
+    if status == True:
+        print("Network connections are ok")
+    else:
+        print("Network error: "+msg)
+
+#    jobinfo = process_cmd(executable, client,hostname,"Submit")
+    raven_process.submit(dataset)
+
+    job_finished = False
+    while(not job_finished):
+
+        time.sleep(60)
+        try:
+
+            out = raven_process.monitor()
+            if out == "PENDING":
+                print("Still pending")
+            if out == "RUNNING":
+                print("Running")
+            if out == "DONE":
+                job_finished = True
+        except Exception as e:
+            print("Exception @monitor")
+            print(e)
+            job_finished = True
+
+    raven_process.retrieve(out_dir)
+    raven_process.cleanup()
+
+    print("done.")
+
+
+
+
 def mainfct(argv):
     #        print("main.py command (Submit|Monitor|Retrieve)")
 
@@ -270,7 +471,6 @@ def mainfct(argv):
 
 
     hosts = [hostname]
-    client = ParallelSSHClient(hosts, user=user)
 
 
     #global output_folder
@@ -309,4 +509,4 @@ def mainfct(argv):
 if __name__ == '__main__':
 
 
-    mainfct(sys.argv[1:])
+    newmainfct(sys.argv[1:])
