f278886f09c8c6dd756deb00c67e7f25b78453fd
richardarsenault <39815445+richardarsenault@users.noreply.github.com>
2019-01-11 16:50:09 -0500

    Add files via upload

---
+++ raven/processes/regionalization_tools.py
@@ -0,0 +1,256 @@
+
+"""
+regionalization_tools.py
+
+Provide various tools for hydrological regionalization.
+"""
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from netCDF4 import Dataset
+
+import logging
+LOGGER = logging.getLogger("PYWPS")
+
+
+
+def get_array_of_gauged_properties(hydrological_model):
+    """
+    get_array_or_gauged_properties
+
+    INPUTS:
+        hydrological_model -- String of the selected hydrological model
+
+    This function return an array of gauged catchments properties as well as
+    model NSE and optimal parameters of the hydrological model.
+    """
+    
+    # Load general gauged catchment properties and create dataframe df.
+    gauged_properties=pd.read_csv(Path(__file__).parent.parent.parent / 'tests/testdata/regionalisation_data/gauged_catchment_properties.csv',dtype=float)
+    types_dict = {'NAME':str, 'STATE':str, 'COUNTRY':str}
+    for col, col_type in types_dict.items():
+       gauged_properties[col] = gauged_properties[col].astype(col_type)
+
+    gauged_properties=gauged_properties.drop('ID', 1)
+    gauged_properties=gauged_properties.drop('NAME', 1)
+    gauged_properties=gauged_properties.drop('STATE', 1)
+    gauged_properties=gauged_properties.drop('COUNTRY', 1)
+    
+    # Get data for the hydrological_model
+    filepath= str(Path(__file__).parent.parent.parent) + '/tests/testdata/regionalisation_data/'+ hydrological_model + '_parameters.csv'
+    model_parameters=pd.read_csv(filepath,dtype=float)
+    model_parameters=model_parameters.drop('ID', 1)
+    
+    # Make sure that the matrices are the same length, else raise error.
+    if np.size(gauged_properties, 0) != np.size(model_parameters, 0):
+        raise ValueError("gauged_properties and model_parameters for \
+                         hydrological model " + hydrological_model +
+                         " do not have the same number of gauged catchments. \
+                         Check for mismatch.")
+
+    return gauged_properties, model_parameters
+
+
+def filter_array_by_NSE(gauged_properties, model_parameters, min_NSE):
+    """
+    filter_array_by_NSE
+
+    INPUTS:
+        gauged_properties -- Properties of all gauged catchments available
+        model_parameters -- Hydrological model calibrated parameters
+        min_NSE -- Minimum Nash-Sutcliffe (NSE) value to select a catchment
+
+    Function to filter the arrays of gauged catchments properties and model
+    parameters according to minimum NSE required by the user.
+    """
+    
+    gauged_properties_filtered = \
+        gauged_properties.loc[model_parameters['NASH'] >= min_NSE]
+    model_parameters_filtered = \
+        model_parameters.loc[model_parameters['NASH'] >= min_NSE]
+    gauged_properties_filtered.reset_index(inplace=True)
+    model_parameters_filtered.reset_index(inplace=True)
+
+    return gauged_properties_filtered, model_parameters_filtered
+
+
+def rank_donor_catchments(ungauged_properties,
+                          model_parameters,
+                          gauged_properties,
+                          properties_to_use,
+                          rank_type='distance'):
+    """
+    rank_donor_catchments
+
+    INPUTS:
+        model_parameters -- Selected hydrological model calibrated parameters
+        ungauged_properties -- Properties of the ungauged catchment
+        rank_type -- Regionalization method for the ranking of the catchments
+        properties_to_use -- The catchments' properties to use if
+                             rank_type='Similarity'
+
+    Function to sort arrays according to the type of sorting (distance or
+    similarity) selected by the user.
+    """
+
+    # Calculate distance between ungauged and database of gauged catchments.
+    dist = np.empty([gauged_properties.shape[0], 1])
+ 
+    
+    # For the spatial distance:
+    if (rank_type == 'distance'):
+        # Calculate the spatial distance between the donor catchments and the
+        # ungauged catchment
+ 
+        
+        for i in range(0, gauged_properties.shape[0]):
+            dist[i] = ((gauged_properties.CENTROID_LONGITUDE[i] -
+                        ungauged_properties.CENTROID_LONGITUDE)**2 +
+                       (gauged_properties.CENTROID_LATITUDE[i] -
+                        ungauged_properties.CENTROID_LATITUDE)**2)**0.5
+
+        # Add the physical distance to the dataframe
+        gauged_properties = gauged_properties.assign(distance=dist)
+
+    # For the physical similarity:
+    elif (rank_type == 'similarity'):
+        # Get the values from the arrays, convert to numpy array
+        similar_properties = gauged_properties[properties_to_use].values
+        similar_targets = np.tile(
+                            ungauged_properties[properties_to_use].values,
+                            (similar_properties.shape[0], 1))
+
+        # Calculate deltas in the database
+        delta_max = np.amax(similar_properties, axis=0, keepdims=True)
+        delta_min = np.amin(similar_properties, axis=0, keepdims=True)
+        deltas = delta_max - delta_min
+
+        # Compute similarity distance
+        dist = np.sum(abs(similar_properties - similar_targets) / deltas, 1)
+        gauged_properties = gauged_properties.assign(distance=dist)
+
+    else:
+        raise ValueError("Regionalization method " + rank_type +
+                         " not recognized")
+
+    # The distance is now in the vectors. It has to be sorted, keep the index
+    # and also sort the model_parameters matrix
+    gauged_properties = gauged_properties.sort_values(by=['distance'])
+    model_parameters = model_parameters.reindex(gauged_properties.index)
+    gauged_properties.reset_index(drop=True, inplace=True)
+    model_parameters.reset_index(drop=True, inplace=True)
+
+    return gauged_properties, model_parameters
+
+
+def get_ungauged_properties(inputs_file, properties_to_use,latitude,longitude):
+    """
+    get_ungauged_properties
+
+    INPUTS:
+        inputs_file -- The ungauged catchment inputs file to work with
+        properties_to_use -- The catchments' properties to use
+
+    Function to get the ungauged catchment's properties from the netCDF file.
+    """
+    
+    # Read the netCDF file
+    nc_file = Dataset(inputs_file[0], 'r')
+
+    """
+    # For now, until we test for real and pass the parameter
+    properties_to_use = ['CENTROID_LATITUDE',
+                         'CENTROID_LONGITUDE',
+                         'AREA',
+                         'avg_elevation',
+                         'avg_slope',
+                         'land_forest',
+                         'land_grass',
+                         'land_impervious',
+                         'land_urban']
+    """
+    # Defile the ungauged catchment properties
+    ungauged_properties = pd.DataFrame({'CENTROID_LATITUDE':[latitude],'CENTROID_LONGITUDE':[longitude]})
+
+
+    return ungauged_properties
+
+
+def IDW(Qsim, number_donors, distance):
+    """
+    IDW - Inverse Distance Weighting
+
+    INPUTS:
+        Qsim -- Simulated streamflow
+        number_donors -- The number of gauged catchments to use
+        distance -- The distance between the ungauged and the gauged catchments
+
+    Function to calculate and weight the Qsim by the distance factor
+    """
+    
+    # In IDW, weights are 1 / distance
+    weights = 1.0 / distance[0:number_donors]
+
+    # Make weights sum to one
+    weights /= weights.sum(axis=0)
+
+    # Calculate weighted average
+    best_estimate = np.dot(Qsim, weights)
+
+    return best_estimate
+
+
+def MLR(ungauged_properties,
+        model_parameters,
+        gauged_properties,
+        properties_to_use):
+    """
+    MLR - Multiple Linear Regression
+
+    INPUTS:
+        ungauged_properties -- Properties of the ungauged catchment
+        model_parameters -- Selected hydrological model calibrated parameters
+        gauged_properties -- Properties of all gauged catchments available
+        properties_to_use -- The catchments' properties to use
+
+    Function to compute regionalization using MLR.
+    """
+
+    # Define X predictors and Y targets for parameters
+    properties_to_use_with_const = properties_to_use[:]
+    # Add the constant column for the intercept
+    properties_to_use_with_const.insert(0, 'const')
+    X = gauged_properties[properties_to_use]
+    # Add the constant 1 for the ungauged catchment predictors
+    predictors=sm.add_constant(ungauged_properties,prepend=True,has_constant='add')
+
+    # Add constants to the gauged predictors
+    X = sm.add_constant(X)
+    MLR_parameters = []
+    r_squared = []
+
+    # Convert to matrix
+    model_parameters = model_parameters.iloc[:, :].values
+
+    # Loop each parameter
+    for j in range(2, model_parameters.shape[1]):
+        
+        # Define predictands and fit statistical model
+        Y = model_parameters[:, j]
+        regression_model = sm.OLS(Y, X).fit()
+        
+        # Collect R-squared (adjusted) and estimated parameters for the gauged
+        # catchment
+        r_squared.append(regression_model.rsquared_adj)
+        tmp=regression_model.predict(exog=predictors)
+        MLR_parameters.append(tmp[0])
+
+    # Convert to arrays
+    MLR_parameters=np.array(MLR_parameters)
+    r_squared=np.array(r_squared)
+    
+    
+    return MLR_parameters, r_squared
+
+
---
+++ raven/processes/wps_regionalisation.py
@@ -0,0 +1,273 @@
+
+"""
+entrypoint_regionalization.py
+
+INPUTS:
+    hydrological_model -- The hydrological model to use, limited as of now to:
+                          'HMETS', 'MOHYSE', 'GR4J'.
+    inputs_file -- Ungauged catchment inputs file containing the following fields:
+        rain (extracted from boundaries beforehand)
+        snow (extracted from boundaries beforehand)
+        tasmax (extracted from boundaries beforehand)
+        tasmin (extracted from boundaries beforehand)
+        surface_area (extracted from GIS beforehand)
+        day of year (extracted from dataset used for meteo)
+        dates (extracted from dataset used for meteo)
+        latitude (centroid, extracted from GIS beforehand)
+        longitude (centroid, extracted from GIS beforehand)
+        land cover fractions (determined by GIS tools beforehand)
+        avg slope (determined by GIS beforehand)
+        avg elevation (determined by GIS beforehand)
+
+    regionalization_method -- An integer with the value-argument pairs:
+        1- Multiple Linear Regression (MLR)
+        2- Spatial Proximity (SP)
+        3- Spatial Proximity with Inverse Distance Weighting (IDW) of
+           donor basins (SP_IDW)
+        4- Spatial Proximity with IDW and regression-based augmentation (RA)
+            (SP_IDW_RA)
+        5- Physical Similarity (PS)
+        6- Physical Similarity with IDW of donor basins (PS_IDW)
+        7- Physical Similarity with IDW and RA (PS_IDW_RA)
+
+    number_donors -- A positive integer from [1:10] that allows averaging the
+        hydrographs of number_donors donors. For example, a value of 5
+        indicates that we take the average hydrograph of the 5 closest or most
+        similar donors. No impact when used for MLR. IDW computes the average 
+        using inverse weighting.
+
+    min_NSE -- The minimum calibration NSE required for a donor catchment to be
+        considered. If the closest or most similar catchment has an NSE below
+        the min_NSE, then it is skipped and the next closest/similar catchment
+        is selected.
+
+OUTPUT:
+    This function returns two arrays. Qsim = [n_days x number_donors] 
+    in which the columns are the individual hydrographs used to produce the 
+    averaged hydrograph, in order of closest to farthest (or most to least similar)
+    The second is "best_estimate", which is the average (or IDW average) of the 
+    Qsim hydrographs.
+"""
+
+
+from . import wpsio as wio
+import logging
+from pywps import Process
+import numpy as np
+from netCDF4 import Dataset
+
+LOGGER = logging.getLogger("PYWPS")
+
+from . regionalization_tools import (
+        get_array_of_gauged_properties,
+        filter_array_by_NSE,
+        rank_donor_catchments,
+        get_ungauged_properties,
+        IDW
+)
+
+from . regionalization_methods import (
+        MLR_parameters_regionalization,
+        distance_only_regionalization,
+        distance_MLR_regionalization
+)
+
+
+
+class RegionalisationProcess(Process):
+    identifier = 'RegionalisationProcess'
+    abstract = 'Regionalisation methods to predict streamflow at ungauged locations'
+    title = "Process to predict streamflow at ungauged sites based on surrounding or similar gauged catchments"
+    version = '0.1'
+
+    inputs = [wio.ts, wio.start_date, wio.end_date, wio.area, wio.elevation, wio.latitude, wio.longitude, wio.model_name, wio.number_donors, wio.min_NSE, wio.regionalisationMethod]
+
+    outputs = [wio.hydrograph]
+
+    
+
+    def __init__(self):
+       
+
+
+        super(RegionalisationProcess, self).__init__(
+            self._handler,
+            identifier="RegionalisationProcess",
+            title="Methods to predict streamflow at ungauged sites",
+            version="0.1",
+            abstract="Use this method to generate streamflow at ungauged sites using a rich database of catchments over North America.",
+            metadata=[],
+            inputs=self.inputs,
+            outputs=self.outputs,
+            status_supported=True,
+            store_supported=True)
+       
+        
+
+    def _handler(self, request, response):
+
+       
+        
+        response.update_status('PyWPS process {} started.'.format(self.identifier), 0)
+        
+      
+        # Model name
+        if 'model_name' in request.inputs:
+            model_name=request.inputs['model_name'][0].data
+        else:
+            raise ValueError('Model Name is not optional')
+
+        if model_name not in ['GR4JCN','HMETS','MOHYSE']: 
+            raise ValueError("Hydrological model " + model_name + " is not supported for regionalization.")
+
+        #Regionalisation Method
+         
+        if 'regionalisationMethod' in request.inputs:
+            regionalisationMethod=request.inputs['regionalisationMethod'][0].data
+        else:
+            regionalisationMethod='SP_IDW'
+            print('regionalisationMethod not provided, set to SP_IDW by default')
+             
+        # Number Donors
+        if 'number_donors' in request.inputs:
+            number_donors=request.inputs['number_donors'][0].data
+        else:
+            number_donors=5
+            print('Number of donors not provided, set to 5 by default')
+
+        # Minimum NSE filter
+        if 'min_NSE' in request.inputs:
+            min_NSE=request.inputs['min_NSE'][0].data
+        else:
+            min_NSE=0.6
+            print('minimum NSE not provided, set to 0.6 by default')
+
+        # Model inputs
+        ts=[f.file for f in request.inputs['ts']]
+
+        
+        ncfilename=request.inputs['ts'][0].file
+        start_date=request.inputs['start_date'][0].data
+        end_date=request.inputs['end_date'][0].data
+        latitude=request.inputs['latitude'][0].data
+        longitude=request.inputs['longitude'][0].data
+        elevation=request.inputs['elevation'][0].data
+        area=request.inputs['area'][0].data
+        
+        # Find the properties we wish to use for multiple linear regression (MLR)
+        # and similarity.
+        """
+        properties_to_use = ['centroid_latitude',
+                         'centroid_longitude',
+                         'surface_area',
+                         'avg_elevation',
+                         'avg_slope',
+                         'land_forest',
+                         'land_grass',
+                         'land_impervious',
+                         'land_urban']
+        """
+        properties_to_use=['CENTROID_LATITUDE','CENTROID_LONGITUDE']
+        # Get the ungauged catchment properties from the inputs_file for the
+        # regionalization scheme.
+        ungauged_properties = get_ungauged_properties(ts,properties_to_use,latitude,longitude)
+        
+
+        # Load gauged catchments properties and hydrological model parameters.
+        gauged_properties, model_parameters = get_array_of_gauged_properties(
+                                            model_name)
+
+        # Filter arrays according to the minimum value allowed.
+        gauged_properties, model_parameters = filter_array_by_NSE(
+                                            gauged_properties,
+                                            model_parameters,
+                                            min_NSE)
+
+        # Check to see if we have enough data, otherwise raise error
+        if (gauged_properties.shape[0] < number_donors and
+                regionalisationMethod != 'MLR'):
+            raise ValueError("hydrological_model and minimum NSE threshold \
+                             combination is too strict for the number of donor \
+                             basins. Please reduce the number of donor basins OR \
+                             reduce the minimum NSE threshold")
+
+        # Rank the matrix according to the similarity or distance.
+        if (regionalisationMethod == 'PS' or regionalisationMethod == 'PS_IDW' or regionalisationMethod == 'PS_IDW_RA' ):  # physical similarity
+            regionalisation_tag = 'similarity'
+        else:
+            # Filter by distance if MLR (option 1) is selected, it is faster
+            # than by similarity.
+            regionalisation_tag = 'distance'
+
+
+            # Create a dataframe that is identical to gauged_properties arrays.
+        gauged_properties, model_parameters = rank_donor_catchments(
+                                              ungauged_properties,
+                                              model_parameters,
+                                              gauged_properties,
+                                              properties_to_use,
+                                              regionalisation_tag
+                                              )
+
+        if regionalisationMethod == 'MLR':
+            # Multiple linear regression (MLR)
+            Qsim = MLR_parameters_regionalization(ncfilename,start_date, end_date, latitude, longitude, elevation, area,
+                                                  model_name, ts,
+                                                  ungauged_properties,
+                                                  model_parameters,
+                                                  gauged_properties,
+                                                  properties_to_use)
+            best_estimate = Qsim
+
+        elif (regionalisationMethod == 'SP') or (regionalisationMethod == 'PS'):
+            # Simple averaging
+            Qsim = distance_only_regionalization(ncfilename,start_date, end_date, latitude, longitude, elevation, area,
+                                                 model_name,
+                                                 ts,
+                                                 number_donors,
+                                                 model_parameters)
+            best_estimate = np.average(Qsim, 1)
+
+        elif (regionalisationMethod == 'SP_IDW') or (regionalisationMethod == 'PS_IDW'):
+            # Inverse distance weighting (IDW)
+            Qsim = distance_only_regionalization(ncfilename,start_date, end_date, latitude, longitude, elevation, area,
+                                                 model_name,
+                                                 ts,
+                                                 number_donors,
+                                                 model_parameters)
+         
+            
+            best_estimate = IDW(Qsim, number_donors, gauged_properties['distance'])
+
+        elif (regionalisationMethod == 'SP_IDW_RA') or (regionalisationMethod == 'PS_IDW_RA'):
+            # Inverse distance weighting with regression-augmentation (IDW_RA)
+            Qsim = distance_MLR_regionalization(ncfilename,start_date, end_date, latitude, longitude, elevation, area,
+                                                model_name, ts,
+                                                number_donors,
+                                                ungauged_properties,
+                                                model_parameters,
+                                                gauged_properties,
+                                                properties_to_use)
+            
+            best_estimate = IDW(Qsim, number_donors, gauged_properties['distance'])
+
+        else:
+            print("Unknown regionalization method")
+
+
+        # Write utput
+        indexfile = 'RegionalisationHydrograph.nc'
+     
+        with Dataset(indexfile, 'w', format='NETCDF4') as nc_file:
+            nc_file.createDimension('time', Qsim.shape[0])
+            nc_file.createDimension('donor_number', Qsim.shape[1])
+
+            vars_list = nc_file.createVariable('Qsim', 'float32', ('time', 'donor_number'))
+            vars_list[:] = Qsim
+            vars_list2=nc_file.createVariable('BestEstimate','float32',('time'))
+            vars_list2[:]=best_estimate
+      
+        
+        response.outputs['hydrograph'].file=indexfile
+        return response
+    
