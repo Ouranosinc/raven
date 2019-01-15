d6889c5820bc0db65972057753225b690c399d77
David Huard <david.huard@gmail.com>
2019-01-15 12:41:08 -0500

    fixed similarity

--- tests/test_regionalisation.py
+++ tests/test_regionalisation.py
@@ -1,5 +1,16 @@
 from raven.utilities import regionalization as reg
-
+import datetime as dt
+from .common import TESTDATA
 
 def test_regionalization():
-    reg.regionalize('MLR', 'GR4JCN', 45, 45, )
+    reg.regionalize('PS', 'GR4JCN',
+                    start_date=dt.datetime(2000, 1, 1),
+                    end_date=dt.datetime(2002, 1, 1),
+                    name='Salmon',
+                    run_name='test',
+                    area='4250.6',
+                    elevation='843.0',
+                    latitude=40.4848,
+                    longitude=-103.3659,
+                    min_NSE=0.6,
+                    ts=TESTDATA['raven-hmets-nc-ts'])
