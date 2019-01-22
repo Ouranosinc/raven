63d5bc57a9989fe948cd38572d48177391fa634e
David Huard <huard.david@ouranos.ca>
2019-01-22 10:12:08 -0500

    added q_sim property as a view. added version property. fixed unit bug. merged ensemble and params in dataset.

--- tests/test_regionalisation.py
+++ tests/test_regionalisation.py
@@ -10,21 +10,21 @@ def test_regionalization():
     props = reg.read_gauged_properties()[variables]
     ungauged_props = {'longitude': .7, 'latitude': .7}
 
-    qsim, ens, par = reg.regionalize('SP_IDW', model, nash, params,
-                                     props, ungauged_props,
-                                     start_date=dt.datetime(2000, 1, 1),
-                                     end_date=dt.datetime(2002, 1, 1),
-                                     name='Salmon',
-                                     run_name='test',
-                                     area='4250.6',
-                                     elevation='843.0',
-                                     latitude=40.4848,
-                                     longitude=-103.3659,
-                                     min_NSE=0.6,
-                                     size=2,
-                                     ts=TESTDATA['raven-hmets-nc-ts'])
+    qsim, ens = reg.regionalize('SP_IDW', model, nash, params,
+                                props, ungauged_props,
+                                start_date=dt.datetime(2000, 1, 1),
+                                end_date=dt.datetime(2002, 1, 1),
+                                name='Salmon',
+                                run_name='test',
+                                area='4250.6',
+                                elevation='843.0',
+                                latitude=40.4848,
+                                longitude=-103.3659,
+                                min_NSE=0.6,
+                                size=2,
+                                ts=TESTDATA['raven-hmets-nc-ts'])
 
     assert(qsim.max() > 1)
     assert(len(ens) == 2)
     assert 'realization' in ens.dims
-    assert 'param' in par.dims
+    assert 'param' in ens.dims
