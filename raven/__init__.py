b9aa0a35e60ad229578e8e4a9c825f7f7310fda2
David Huard <huard.david@ouranos.ca>
2019-01-30 15:57:50 -0500

    raven and ostrich now working for a test case

--- raven/__init__.py
+++ raven/__init__.py
@@ -9,5 +9,11 @@ __author__ = """David Huard"""
 __email__ = 'huard.david@ouranos.ca'
 __version__ = '0.1.0'
 
-raven_exec = str(Path(__file__).parent.parent / 'bin' / 'raven')
-ostrich_exec = str(Path(__file__).parent.parent / 'bin' / 'raven')
+raven_exec = Path(__file__).parent.parent / 'bin' / 'raven'
+
+if not raven_exec.exists:
+    raise IOError("The raven executable is not installed.")
+
+ostrich_exec = Path(__file__).parent.parent / 'bin' / 'ostrich'
+if not ostrich_exec.exists:
+    raise IOError("The ostrich executable is not installed.")
