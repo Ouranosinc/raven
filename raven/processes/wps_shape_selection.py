cf1072cf3495988549c18a0f3bb400d14d4e2a5f
Trevor James Smith <trevor_smith@live.com>
2019-02-12 16:56:20 -0500

    refactored shape selection and integrated upstream basins output via geopandas

--- raven/processes/wps_shape_selection.py
+++ raven/processes/wps_shape_selection.py
@@ -3,10 +3,11 @@ import logging
 import re
 
 import fiona as fio
+import geopandas as gpd
 from fiona.crs import from_epsg
 from pywps import LiteralInput, ComplexInput, ComplexOutput
 from pywps import Process, FORMATS
-from raven.utils import archive_sniffer
+from raven.utils import archive_sniffer, crs_sniffer
 from shapely.geometry import shape, Point
 
 LOGGER = logging.getLogger("PYWPS")
@@ -35,14 +36,11 @@ class ShapeSelectionProcess(Process):
         ]
 
         outputs = [
-            ComplexOutput('properties', 'Watershed feature properties',
-                          abstract='Descriptions of shape properties.',
-                          supported_formats=[FORMATS.JSON]),
-            ComplexOutput('geometry', 'Watershed feature geometry',
+            ComplexOutput('geojson', 'Watershed feature geometry',
                           abstract='Geographic representations of shape properties.',
                           supported_formats=[FORMATS.GEOJSON]),
-            ComplexOutput('upstream_basins', 'IDs for all immediate upstream basins',
-                          abstract='List of all first-level tributary basins by their HydroBASINS ID',
+            ComplexOutput('upstream_basins', 'HydroBASINS IDs for all immediate upstream basins',
+                          abstract='Exhaustive list of all tributary sub-basins according to their HydroBASINS IDs',
                           supported_formats=[FORMATS.JSON])
         ]
 
@@ -60,20 +58,25 @@ class ShapeSelectionProcess(Process):
 
     def _handler(self, request, response):
 
+        def get_upstream_basins(basin_dataframe, basin_id):
+            up_str_mask = basin_dataframe['NEXT_DOWN'] == basin_id
+            iter_upstream = basin_dataframe[up_str_mask]['HYBAS_ID']
+            return iter_upstream
+
         collect_upstream = request['collect_upstream']
         lonlat = request['lonlat_coordinate']
-        crs = request['crs']
+        crs = from_epsg(request['crs'])
         shape_url = request['shape']
 
         # collect_upstream = request.inputs['collect_upstream'][0].data
         # lonlat = request.inputs['lonlat_coordinate'][0].data
-        # crs = request.inputs['crs'][0].data
+        # crs = from_epsg(request.inputs['crs'][0].data)
         # shape_url = request.inputs['shape'][0].file
 
         try:
             lon, lat = tuple(map(float, re.findall(r'[-+]?[0-9]*\.?[0-9]+', lonlat)))
         except Exception as e:
-            msg = 'Failed to parse geo-coordinates {0}: {1}'.format(lonlat, e)
+            msg = 'Failed to parse geo-coordinates {}: {}'.format(lonlat, e)
             LOGGER.error(msg)
             return response
 
@@ -81,70 +84,82 @@ class ShapeSelectionProcess(Process):
         shape_url = archive_sniffer(shape_url, working_dir=self.workdir, extensions=extensions)
 
         basin = []
-        geometry = []
+        geojson = []
         upstream_basins = []
         properties = []
         location = Point(lon, lat)
+        crs = crs_sniffer(shape_url, crs)
 
-        try:
+        with fio.Env():  # hacky workaround for pip-installed fiona; Can be removed in conda systems
             try:
-                src = fio.open(shape_url, 'r')
+                with fio.open(shape_url, 'r', crs=crs) as src:
+                    for feat in iter(src):
+                        geom = shape(feat['geometry'])
+
+                        if geom.contains(location):
+                            # pfaf = feature['properties']['PFAF_ID']
+                            if collect_upstream:
+                                basin = feat['properties']['HYBAS_ID']
+                            else:
+                                geojson = json.dumps(feat)
+                            continue
+                    src.close()
+
+                if collect_upstream:
+                    with fio.Collection(shape_url, 'r', crs=crs) as src:
+                        gdf = gpd.GeoDataFrame.from_features(src, crs=crs)
+                        row_id = gdf['HYBAS_ID'] == basin
+                        main_basin_id = gdf[row_id]['MAIN_BAS']
+                        main_basin_mask = gdf['MAIN_BAS'] == main_basin_id.values[0]
+                        main_basin_gdf = gdf[main_basin_mask]
+
+                        all_basins = list(main_basin_gdf[row_id]['HYBAS_ID'].values)
+                        for i in all_basins:
+                            tmp = get_upstream_basins(main_basin_gdf, i)
+                            if len(tmp):
+                                all_basins.extend(tmp.values)
+
+                        df_sub = main_basin_gdf[main_basin_gdf['HYBAS_ID'] == all_basins[0]]
+                        for a in all_basins[1:]:
+                            df_sub = df_sub.append(main_basin_gdf[main_basin_gdf['HYBAS_ID'] == a])
+
+                        dissolved = df_sub.dissolve(by='MAIN_BAS')
+                        geojson = dissolved.to_json()
+                        upstream_basins = all_basins
+
+                        # filename = '{}.shp'.format(basin)
+                        # LOGGER.warning('Writing {} to shapefile'.format(filename))
+                        # dissolved.to_file('output.json', driver='GeoJSON')
+
+                    # with fio.open(shape_url, 'r', crs=from_epsg(crs)) as src:
+                    #     # 'PFAF_ID' can also technically be used to described the drainage network. See HydroBASINS docs.
+                    #     # pfaf_start, pfaf_end = str(pfaf)[0:3], str(pfaf)[3:]
+                    #     print(len(src))
+                    #     upstream = filter(lambda f: feature['properties']['NEXT_DOWN'] == basin[0], src)
+
             except Exception as e:
-                msg = '{}: Unable to read crs from {}. Proceeding with crs={}'.format(e, shape_url, crs)
-                LOGGER.warning(msg)
-                src = fio.open(shape_url, 'r', crs=from_epsg(crs))
+                msg = 'Failed to extract shape from url {}: {}'.format(shape_url, e)
+                LOGGER.error(msg)
+                return response
 
-            for feature in iter(src):
-                geom = shape(feature['geometry'])
 
-                if geom.contains(location):
-                    # pfaf = feature['properties']['PFAF_ID']
-
-                    basin = [feature['properties']['HYBAS_ID']]
-                    prop = {'id': feature['id']}
-                    prop.update(feature['properties'])
-                    properties.append(prop)
-                    geometry.append(feature['geometry'])
-                else:
-                    continue
-                src.close()
-
-            # if collect_upstream:
-            #     with fio.open(shape_url, 'r', crs=from_epsg(crs)) as src:
-            #         # 'PFAF_ID' can also technically be used to described the drainage network. See HydroBASINS docs.
-            #         # pfaf_start, pfaf_end = str(pfaf)[0:3], str(pfaf)[3:]
-            #         print(len(src))
-            #         upstream = filter(lambda f: feature['properties']['NEXT_DOWN'] == basin[0], src)
-            #
-            #         for up in iter(upstream):
-            #             print(up, 'test')
-            #             upstream_basins.append(up['properties']['HYBAS_ID'])
-
-        except Exception as e:
-            msg = 'Failed to extract shape from url {}: {}'.format(shape_url, e)
-            LOGGER.error(msg)
-            return response
-
-        # response.outputs['properties'].data = json.dumps(properties)
-        # response.outputs['geometry'].data = json.dumps(geometry)
+        # response.outputs['watershed'].data = json.dumps(geometry)
         # response.outputs['upstream_basins'].data = json.dumps(upstream_basins)
 
-        response.outputs['properties'] = properties
-        response.outputs['geometry'] = geometry
-        response.outputs['upstream_basins'] = upstream_basins
+        response['basin'] = geojson
+        response['upstream_basins'] = upstream_basins
 
         return response
 
 
 if __name__ == '__main__':
-
-    inputs = dict(collect_upstream=True,
+    inputs = dict(collect_upstream=False,
                   lonlat_coordinate="(-68.724444, 50.646667)",
                   crs=4326,
-                  shape='/home/tjs/git/raven/tests/testdata/usgs_hydrobasins/hybas_lake_na_lev12_v1c.zip')
-
+                  shape='/home/tjs/git/raven/tests/testdata/usgs_hydrobasins/hybas_lake_na_lev10_v1c.zip')
     outputs = {}
-
     c = ShapeSelectionProcess()._handler(request=inputs, response=outputs)
+    print(outputs['basin'], outputs['upstream_basins'])
 
-    print(outputs)
+
+
