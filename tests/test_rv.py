610495dc077951abe106e11909e6252db833a613
David Huard <huard.david@ouranos.ca>
2019-02-18 11:19:21 -0500

    added configuration values for random seed

--- tests/test_rv.py
+++ tests/test_rv.py
@@ -1,6 +1,6 @@
 import pytest
 import raven
-from raven.models.rv import RV, RVI, RVFile, isinstance_namedtuple
+from raven.models.rv import RV, RVI, Ost, RVFile, isinstance_namedtuple
 import datetime as dt
 from collections import namedtuple
 from .common import TESTDATA
@@ -96,6 +96,16 @@ class TestRV:
 
         m = Mod(params=Mod.params(1, 2, 3))
         assert m.params.x1 == 1
+
+
+class TestOst:
+    def test_random(self):
+        o = Ost()
+        assert o.comment_random == '#'
+
+        o = Ost(random_seed=0)
+        assert o.comment_random == ''
+        assert o.random_seed == 0
 
 
 def test_isinstance_namedtuple():
