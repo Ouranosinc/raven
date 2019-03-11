61d043b413323bcc4539faf3ac8f5f40c9968e7d
Marc Lalonde <marc.lalonde@crim.ca>
2019-03-11 12:04:12 -0400

    Parameterize hard-coded slurm commands

---
+++ raven/hpc_interface/constants.py
@@ -0,0 +1,6 @@
+
+logging_name = "hpc-pavics"
+logging_fname = "hpclog.txt"
+shub_server = "132.217.141.54"
+squeue_cmd = "/opt/software/slurm/bin/squeue"
+sbatch_cmd = "/opt/software/slurm/bin/sbatch --account=def-dhuard-ab"
