4146b810e76c4651cc25e682ef020f79bae19910
David Huard <huard.david@ouranos.ca>
2019-03-11 17:11:03 -0400

    zipped multiple outputs in multi-model process

--- tests/test_wps_raven_multi_model.py
+++ tests/test_wps_raven_multi_model.py
@@ -15,12 +15,12 @@ class TestRavenMultiModelProcess:
     def test_simple(self):
         client = client_for(Service(processes=[RavenMultiModelProcess(), ], cfgfiles=CFG_FILE))
 
-        gr4j = '0.529, -3.396, 407.29, 1.072, 16.9, 0.947'
+        gr4jcn = '0.529, -3.396, 407.29, 1.072, 16.9, 0.947'
         hmets = '9.5019, 0.2774, 6.3942, 0.6884, 1.2875, 5.4134, 2.3641, 0.0973, 0.0464, 0.1998, 0.0222, -1.0919, ' \
                 '2.6851, 0.3740, 1.0000, 0.4739, 0.0114, 0.0243, 0.0069, 310.7211, 916.1947'
 
         datainputs = "ts=files@xlink:href=file://{ts};" \
-                     "gr4j={gr4j};" \
+                     "gr4jcn={gr4jcn};" \
                      "hmets={hmets};" \
                      "start_date={start_date};" \
                      "end_date={end_date};" \
@@ -30,7 +30,7 @@ class TestRavenMultiModelProcess:
                      "longitude={longitude};" \
                      "elevation={elevation};" \
             .format(ts=TESTDATA['raven-gr4j-cemaneige-nc-ts'],
-                    gr4j=gr4j,
+                    gr4jcn=gr4jcn,
                     hmets=hmets,
                     start_date=dt.datetime(2000, 1, 1),
                     end_date=dt.datetime(2002, 1, 1),
@@ -47,3 +47,5 @@ class TestRavenMultiModelProcess:
             datainputs=datainputs)
 
         assert_response_success(resp)
+        out = get_output(resp.xml)
+        assert out['hydrograph'].endswith('.zip')
