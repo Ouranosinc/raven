6821dcebb386c9b2b5d6e69d4e1251aabea7a112
Marc Lalonde <marc.lalonde@crim.ca>
2019-03-12 08:53:08 -0400

    Improve job status mngmt

--- raven/hpc_interface/constants.py
+++ raven/hpc_interface/constants.py
@@ -2,5 +2,6 @@
 logging_name = "hpc-pavics"
 logging_fname = "hpclog.txt"
 shub_server = "132.217.141.54"
-squeue_cmd = "/opt/software/slurm/bin/squeue"
-sbatch_cmd = "/opt/software/slurm/bin/sbatch --account=def-dhuard-ab"
+squeue_cmd = "/opt/software/slurm/bin/sacct"
+sbatch_cmd = "/opt/software/slurm/bin/sbatch"
+scancel_cmd = "/opt/software/slurm/bin/scancel"
