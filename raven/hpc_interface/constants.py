fc7cc3a4361afc15467d29d32bc2d2e07c3dd7f5
Marc Lalonde <marc.lalonde@crim.ca>
2019-03-12 09:55:56 -0400

    Move hardcoded constants to constants.py; expected job duration is now a parameter

--- raven/hpc_interface/constants.py
+++ raven/hpc_interface/constants.py
@@ -2,6 +2,7 @@
 logging_name = "hpc-pavics"
 logging_fname = "hpclog.txt"
 shub_server = "132.217.141.54"
+cc_account_info = "def-dhuard-ab"
 squeue_cmd = "/opt/software/slurm/bin/sacct"
 sbatch_cmd = "/opt/software/slurm/bin/sbatch"
 scancel_cmd = "/opt/software/slurm/bin/scancel"
