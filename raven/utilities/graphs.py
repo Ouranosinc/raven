eb9063ca54da3be7c3ee3f9dd0be4718cebcd759
richardarsenault <39815445+richardarsenault@users.noreply.github.com>
2019-03-14 11:43:34 -0400

    Added filename-passing ability for graph legends

--- raven/utilities/graphs.py
+++ raven/utilities/graphs.py
@@ -1,25 +1,28 @@
 from matplotlib import pyplot as plt
 import xarray as xr
 
-
-def ensemble_uncertainty(fns):
+#TODO: Ensure that the nameList items are in the same order as the generator netCDF files!!!
+    
+def ensemble_uncertainty_annual(fns,nameList):
     """Create a graphic of the mean hydrological cycle for each model simulation.
 
     Parameters
     ----------
     fns : sequence
       Files storing raven hydrographs.
-
+    nameList: list
+      List of strings of the model runs to add to the legend.
     Returns
     -------
     fig : plt.Figure
       Matplotlib figure showing the mean annual cycle for each simulation.
     """
     ds = [xr.open_dataset(fn) for fn in fns]
-    q_sims = [h.q_sim for h in ds]
-    ahs = [q.groupby('time.dayofyear').mean() for q in q_sims]
+    simulated_streamflow = [h.q_sim for h in ds]
+    ahs = [q.groupby('time.dayofyear').mean() for q in simulated_streamflow]
 
     fig, ax = plt.subplots(1, 1)
     [ah.plot(ax=ax) for ah in ahs]
+    ax.legend(nameList)
 
     return fig
