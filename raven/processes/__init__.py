e7db36504bfa7be5b5f0a42dd3366a2d7a5d4505
richardarsenault <39815445+richardarsenault@users.noreply.github.com>
2019-03-26 18:24:39 -0400

    Added WPS to raph single hydrograph + examples for multimodel

--- raven/processes/__init__.py
+++ raven/processes/__init__.py
@@ -4,6 +4,11 @@ from .wps_raven_gr4j_cemaneige import RavenGR4JCemaNeigeProcess
 from .wps_raven_mohyse import RavenMOHYSEProcess
 from .wps_raven_hmets import RavenHMETSProcess
 from .wps_raven_hbv_ec import RavenHBVECProcess
+from .wps_generic_shape_properties import ShapePropertiesProcess
+from .wps_hydrobasins_shape_selection import ShapeSelectionProcess
+from .wps_generic_raster_subset import RasterSubsetProcess
+from .wps_generic_terrain_analysis import TerrainAnalysisProcess
+from .wps_generic_zonal_stats import ZonalStatisticsProcess
 from .wps_ostrich_gr4j_cemaneige import OstrichGR4JCemaNeigeProcess
 from .wps_ostrich_mohyse import OstrichMOHYSEProcess
 from .wps_ostrich_hmets import OstrichHMETSProcess
@@ -12,14 +17,20 @@ from .wps_objective_functions import ObjectiveFunctionProcess
 from .wps_regionalisation import RegionalisationProcess
 from .wps_raven_multi_model import RavenMultiModelProcess
 from .wps_graph_ensemble_uncertainty import GraphEnsUncertaintyProcess
+from .wps_graph_single_hydrograph import GraphSingleHydrographProcess
 
 processes = [
     RavenProcess(),
     GR4JCemaNeigeProcess(),
+    RasterSubsetProcess(),
     RavenGR4JCemaNeigeProcess(),
     RavenMOHYSEProcess(),
     RavenHMETSProcess(),
     RavenHBVECProcess(),
+    ShapePropertiesProcess(),
+    ShapeSelectionProcess(),
+    TerrainAnalysisProcess(),
+    ZonalStatisticsProcess(),
     OstrichGR4JCemaNeigeProcess(),
     OstrichMOHYSEProcess(),
     OstrichHMETSProcess(),
@@ -27,5 +38,6 @@ processes = [
     ObjectiveFunctionProcess(),
     RavenMultiModelProcess(),
     GraphEnsUncertaintyProcess(),
+    GraphSingleHydrographProcess()
     # RegionalisationProcess(),
 ]
--- raven/processes/wps_graph_single_hydrograph.py
+++ raven/processes/wps_graph_single_hydrograph.py
@@ -1,16 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 14:31:29 2019
+
+@author: ets
+"""
+
 from pywps import ComplexInput, ComplexOutput
 from pywps import Format
+from pywps import FORMATS
 from pywps import Process
-import zipfile
+
 from pathlib import Path
-from raven.utilities.graphs import mean_annual_hydrograph, hydrograph
+from raven.utilities.graphs import mean_annual_hydrograph, hydrograph, spaghetti_annual_hydrograph
 from matplotlib import pyplot as plt
 
-class GraphEnsUncertaintyProcess(Process):
+class GraphSingleHydrographProcess(Process):
     def __init__(self):
-        inputs = [ComplexInput('sims', 'Stream flow simulations ensemble',
+        inputs = [ComplexInput('sim', 'Stream flow simulations NetCDF File',
                                abstract='Stream flow simulation time series',
-                               supported_formats=(Format(mime_type='application/zip'),)),
+                               supported_formats=[FORMATS.NETCDF]),
                   ]
 
         outputs = [ComplexOutput('graph_ensemble_hydrographs', 'Figure showing the simple hydrographs of the included models.',
@@ -22,11 +31,16 @@ class GraphEnsUncertaintyProcess(Process):
                                  abstract="",
                                  as_reference=True,
                                  supported_formats=(Format(mime_type='image/png'), )),
+                   
+                   ComplexOutput('graph_spaghetti_hydrographs', 'Figure showing the annual hydrographs for each year.',
+                                 abstract="",
+                                 as_reference=True,
+                                 supported_formats=(Format(mime_type='image/png'), )),
                    ]
 
-        super(GraphEnsUncertaintyProcess, self).__init__(
+        super(GraphSingleHydrographProcess, self).__init__(
             self._handler,
-            identifier="graph_ensemble_uncertainty",
+            identifier="graph_single_hydrograph",
             title="",
             version="1.0",
             abstract="",
@@ -38,26 +52,29 @@ class GraphEnsUncertaintyProcess(Process):
             store_supported=True)
 
     def _handler(self, request, response):
-        sim_fn = request.inputs['sims'][0].file
-
-        # Extract files from archive in temp directory
-        tmp = Path(self.workdir) / 'sims'
-        with zipfile.ZipFile(sim_fn) as z:
-            z.extractall(tmp)
-
-        # Create and save graphic  
+        
        
-        fig = mean_annual_hydrograph(tmp.glob('*.nc'))
-        fig_fn_annual = Path(self.workdir) / 'ensemble_annual_hydrographs.png'
+        sim_fn = Path(request.inputs['sim'][0].workdir).glob('*.nc')
+        
+        # Create and save graphics  
+        fig = mean_annual_hydrograph(sim_fn)
+        fig_fn_annual = Path(self.workdir) / 'single_annual_hydrographs.png'
         fig.savefig(fig_fn_annual)
         plt.close(fig)
+    
+        sim_fn = Path(request.inputs['sim'][0].workdir).glob('*.nc')
         
-        fig = hydrograph(tmp.glob('*.nc'))
-        fig_fn_simple = Path(self.workdir) / 'simple_hydrographs.png'
+        fig = hydrograph(sim_fn)
+        fig_fn_simple = Path(self.workdir) / 'simple_hydrograph.png'
         fig.savefig(fig_fn_simple)
         plt.close(fig)
-    
-        
+     
+        sim_fn = request.inputs['sim'][0].file
+
+        fig = spaghetti_annual_hydrograph(sim_fn)
+        fig_fn_spag = Path(self.workdir) / 'spaghetti_hydrographs.png'
+        fig.savefig(fig_fn_spag)
+        plt.close(fig)
   #      zipPath=Path(self.workdir) / 'ensemble_hydrographs.zip'
   #      with zipfile.ZipFile(zipPath,'w') as zip: 
             
@@ -66,4 +83,5 @@ class GraphEnsUncertaintyProcess(Process):
   #      #TODO: ZIP FIGURE FILES TOGETHER
         response.outputs['graph_ensemble_hydrographs'].file = str(fig_fn_simple)
         response.outputs['graph_annual_hydrographs'].file = str(fig_fn_annual)
+        response.outputs['graph_spaghetti_hydrographs'].file = str(fig_fn_spag)
         return response
