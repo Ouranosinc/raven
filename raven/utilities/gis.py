ae311b3d90d590d470dfda830b8231ac78f6d109
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2019-05-09 11:56:09 -0400

    WFS feature requests (#118)
    * Added WFS feature requests for HydroBASINS data
    
    * Split hydrobasins functions and leveraged GeoPandas WFS capabilities
    
    * Added requests to requirements.txt
    
    * Docstring adjustments

--- raven/utilities/gis.py
+++ raven/utilities/gis.py
@@ -1,5 +1,6 @@
 import fiona
 import collections
+
 from raven.utils import crs_sniffer, single_file_check
 from shapely.geometry import shape, Point
 
@@ -88,7 +89,7 @@ def hydrobasins_aggregate(gdf):
     ----------
     ids : sequence
       Basins ids, namely the HYBAS_ID attribute.
-    df : pd.DataFrame
+    gdf : pd.DataFrame
       Watershed attributes indexed by HYBAS_ID
 
     Returns
@@ -103,6 +104,9 @@ def hydrobasins_aggregate(gdf):
             return x.sum()
         else:
             return x[0]
+
+    # Buffer function to fix invalid geometries
+    gdf['geometry'] = gdf.buffer(0)
 
     return gdf.dissolve(by='MAIN_BAS', aggfunc=aggfunc)
 
@@ -134,7 +138,7 @@ def get_bbox(vector, all_features=True):
         return src.bounds
 
 
-def get_raster_wcs(bbox, geographic=True, layer=None):
+def get_raster_wcs(coordinates, geographic=True, layer=None):
     """Return a subset of a raster image from the local GeoServer via WCS 2.0.1 protocol.
 
     For geoggraphic rasters, subsetting is based on WGS84 (Long, Lat) boundaries. If not geographic, subsetting based
@@ -142,8 +146,8 @@ def get_raster_wcs(bbox, geographic=True, layer=None):
 
     Parameters
     ----------
-    bbox : sequence
-      Geographic coordinates of the bounding box (lon0, lat0, lon1, lat1)
+    coordinates : sequence
+      Geographic coordinates of the bounding box (left, down, right, up)
     geographic : bool
       If True, uses "Long" and "Lat" in WCS call. Otherwise uses "E" and "N".
     layer : str
@@ -158,7 +162,7 @@ def get_raster_wcs(bbox, geographic=True, layer=None):
     from owslib.wcs import WebCoverageService
     from lxml import etree
 
-    (left, down, right, up) = bbox
+    (left, down, right, up) = coordinates
 
     if geographic:
         x, y = 'Long', 'Lat'
@@ -185,3 +189,97 @@ def get_raster_wcs(bbox, geographic=True, layer=None):
     except etree.XMLSyntaxError:
         # The response is the DEM array.
         return data
+
+
+def get_hydrobasins_location_wfs(coordinates=None, level=12, lakes=True):
+    """Return features from the USGS HydroBASINS data set using bounding box coordinates and WFS 1.1.0 protocol.
+
+    For geographic rasters, subsetting is based on WGS84 (Long, Lat) boundaries. If not geographic, subsetting based
+    on projected coordinate system (Easting, Northing) boundaries.
+
+    Parameters
+    ----------
+    coordinates : sequence
+      Geographic coordinates of the bounding box (left, down, right, up)
+    level : int
+      Level of granularity requested for the lakes vector (1:12). Default: 12.
+    lakes : bool
+      Whether or not the vector should include the delimitation of lakes.
+
+    Returns
+    -------
+    bytes
+      A GML-encoded vector feature.
+
+    """
+    from owslib.wfs import WebFeatureService
+
+    layer = 'public:USGS_HydroBASINS_{}na_lev{}'.format('lake_' if lakes else '', level)
+
+    if coordinates is not None:
+        wfs = WebFeatureService('http://boreas.ouranos.ca/geoserver/wfs', version='1.1.0', timeout=30)
+        try:
+            resp = wfs.getfeature(typename=layer, bbox=coordinates, srsname='urn:x-ogc:def:crs:EPSG:4326')
+        except Exception as e:
+            raise Exception(e)
+    else:
+        raise NotImplementedError
+
+    data = resp.read()
+    return data
+
+
+def get_hydrobasins_attributes_wfs(attribute=None, value=None, level=12, lakes=True):
+    """Return features from the USGS HydroBASINS data set using attribute value selection and WFS 1.1.0 protocol.
+
+    For geographic rasters, subsetting is based on WGS84 (Long, Lat) boundaries. If not geographic, subsetting based
+    on projected coordinate system (Easting, Northing) boundaries.
+
+    Parameters
+    ----------
+    attribute : str
+      Attribute/field to be queried.
+    value: str or float or int
+      Value for attribute queried.
+    level : int
+      Level of granularity requested for the lakes vector (1:12). Default: 12.
+    lakes : bool
+      Whether or not the vector should include the delimitation of lakes.
+
+    Returns
+    -------
+    str
+      URL to the GeoJSON-encoded WFS response.
+
+    """
+    from requests import Request
+    from owslib.fes import PropertyIsLike
+    from lxml import etree
+
+    url = 'http://boreas.ouranos.ca/geoserver/wfs'
+    layer = 'public:USGS_HydroBASINS_{}na_lev{}'.format('lake_' if lakes else '', level)
+
+    if attribute is not None and value is not None:
+
+        try:
+            attribute = str(attribute)
+            value = str(value)
+
+        except ValueError:
+            raise Exception('Unable to cast attribute/filter to string')
+
+        try:
+            filter_request = PropertyIsLike(propertyname=attribute, literal=value, wildCard='*')
+            filterxml = etree.tostring(filter_request.toXML()).decode('utf-8')
+            params = dict(service='WFS', version='1.1.0', request='GetFeature', typename=layer, outputFormat='json',
+                          filter=filterxml)
+
+            q = Request('GET', url, params=params).prepare().url
+
+        except Exception as e:
+            raise Exception(e)
+
+    else:
+        raise NotImplementedError
+
+    return q
