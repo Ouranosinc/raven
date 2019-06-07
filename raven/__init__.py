09a4c4f18c2c9e7b15f72d93255d31c274bb6515
David Huard <huard.david@ouranos.ca>
2019-06-07 13:33:38 -0400

    Bump version: 0.6.1 → 0.6.2

--- raven/__init__.py
+++ raven/__init__.py
@@ -8,7 +8,7 @@ import warnings
 
 __author__ = """David Huard"""
 __email__ = 'huard.david@ouranos.ca'
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 
 raven_exec = Path(__file__).parent.parent / 'bin' / 'raven'
 if not raven_exec.exists():
