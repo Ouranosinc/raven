2c2e20e38182a712e1fb50c70a7588891cc7e4eb
Marc Lalonde <marc.lalonde@crim.ca>
2019-06-15 19:39:03 -0400

    Add some test data for the notebook

---
+++ raven/hpc_interface/test_data/ostrich-mohyse-salmon/save_best.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+echo "saving input files for the best solution found ..."
+
+if [ ! -e best ] ; then
+    mkdir best
+fi
+
+cp model/raven-mohyse-salmon.rvp  best/raven-mohyse-salmon.rvp
+cp model/raven-mohyse-salmon.rvh  best/raven-mohyse-salmon.rvh
+cp model/output/Diagnostics.csv best/Diagnostics.csv 
+cp model/output/Hydrographs.csv best/Hydrographs.csv
+
+exit 0
+
