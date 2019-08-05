6b87eb57f95ef29e80eea8fcda07c48fe97d7c5d
Zeitsperre <trevor_smith@live.com>
2019-08-05 16:08:56 -0400

    First pass at enabling jupyter notebooks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.todo',
               'pywps.ext_autodoc',
               'nbsphinx',
+              'IPython.sphinxext.ipython_console_highlighting',
               ]
 
 autoapi_type = 'python'
@@ -80,7 +81,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
