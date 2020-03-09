0f5a8510d05a9377ba7a95d42cce26e4d071c4e3
David Huard <huard.david@ouranos.ca>
2020-03-09 17:14:09 -0400

    revert changes to how lat lon are parsed to fix notebook

--- raven/processes/wps_hydrobasins_shape_selection.py
+++ raven/processes/wps_hydrobasins_shape_selection.py
@@ -75,7 +75,7 @@ class HydroBasinsSelectionProcess(Process):
         level = 12  # request.inputs['level'][0].data
         lakes = True  # request.inputs['lakes'][0].data
         collect_upstream = request.inputs['aggregate_upstream'][0].data
-        lon, lat = map(float, request.inputs['location'][0].data.split(','))
+        lon, lat = parse_lonlat(request.inputs['location'][0].data)
         bbox = (lon, lat, lon, lat)
 
         shape_url = tempfile.NamedTemporaryFile(prefix='hybas_', suffix='.gml', delete=False,
