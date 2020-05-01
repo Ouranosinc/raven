1cf2d7f87384b905c020fcdd2e506d3c849a4ba6
richardarsenault <richard.arsenault@etsmtl.ca>
2020-04-30 21:35:16 -0400

    commented figure generation and re-added skip era5 tests

--- tests/test_ERA5.py
+++ tests/test_ERA5.py
@@ -5,7 +5,7 @@ import xarray as xr
 
 from pywps import Service
 from pywps.tests import assert_response_success
-import pdb
+
 from . common import client_for, TESTDATA, CFG_FILE, get_output, urlretrieve
 from raven.processes import RavenHMETSProcess
 from raven.models import HMETS
@@ -16,7 +16,7 @@ params = (9.5019, 0.2774, 6.3942, 0.6884, 1.2875, 5.4134, 2.3641, 0.0973, 0.0464
           2.6851, 0.3740, 1.0000, 0.4739, 0.0114, 0.0243, 0.0069, 310.7211, 916.1947)
 
 
-#@pytest.mark.skip(reason="Hanging test due to ncML. Should be removed in a future PR")
+@pytest.mark.skip(reason="Hanging test due to ncML. Should be removed in a future PR")
 class TestRavenERA5:
     def test_simple(self, era5_hr):
         model = HMETS()
@@ -32,11 +32,11 @@ class TestRavenERA5:
               longitude=-123.3659,
               rain_snow_fraction="RAINSNOW_DINGMAN",
               tas={'linear_transform': (1.0, -273.15), 'time_shift': -.25},
-              pr={'linear_transform': (1000*24, 0.0), 'time_shift': -.25}
+              pr={'linear_transform': (1000.0*24.0, 0.0), 'time_shift': -.25}
               )
 
 
-#@pytest.mark.skip(reason="Hanging test due to ncML. Should be removed in a future PR")
+@pytest.mark.skip(reason="Hanging test due to ncML. Should be removed in a future PR")
 class TestRavenERA5Process:
 
     def test_simple(self, era5_hr):
@@ -69,17 +69,17 @@ class TestRavenERA5Process:
                     longitude=-123.3659,
                     rain_snow_fraction="RAINSNOW_DINGMAN",
                     tas=json.dumps({'tas': {'linear_transform': (1.0, -273.15), 'time_shift': -.25}}),
-                    pr=json.dumps({'pr': {'linear_transform': (1000*24, 0.0), 'time_shift': -.25}}),
+                    pr=json.dumps({'pr': {'linear_transform': (1000.0*24.0, 0.0), 'time_shift': -.25}}),
                     )
         
         resp = client.get(
             service='WPS', request='Execute', version='1.0.0', identifier='raven-hmets',
             datainputs=datainputs)
-        pdb.set_trace()
+        
         assert_response_success(resp)
-        out = get_output(resp.xml)
-        tmp,_= urlretrieve(out['hydrograph'])
-        q=xr.open_dataset(tmp)
-        plt.plot(q['q_sim'])
-        plt.show()
- 
+#        out = get_output(resp.xml)
+#        tmp,_= urlretrieve(out['hydrograph'])
+#        q=xr.open_dataset(tmp)
+#        plt.plot(q['q_sim'])
+#        plt.show()
+# 
