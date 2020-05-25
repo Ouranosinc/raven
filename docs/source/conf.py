de47cdd53ba06351e0c8bf4b63e168e9318f84da
Long Vu <vu.long@ouranos.ca>
2020-05-25 13:09:58 -0400

    RtD: fix missing list of processes
    https://readthedocs.org/projects/pavics-raven/builds/11114016/
    
    ```
    WARNING: autodoc: failed to import process 'processes.RavenProcess' from module 'raven'; the following exception was raised:
    No module named 'gdal'
    WARNING: autodoc: failed to import process 'processes.RavenGR4JCemaNeigeProcess' from module 'raven'; the following exception was raised:
    No module named 'gdal'
    WARNING: autodoc: failed to import process 'processes.RavenMOHYSEProcess' from module 'raven'; the following exception was raised:
    No module named 'gdal'
    WARNING: autodoc: failed to import process 'processes.RavenHMETSProcess' from module 'raven'; the following exception was raised:
    No module named 'gdal'
    WARNING: autodoc: failed to import process 'processes.RavenHBVECProcess' from module 'raven'; the following exception was raised:
    No module named 'gdal'
    WARNING: autodoc: failed to import process 'processes.ObjectiveFunctionProcess' from module 'raven'; the following exception was raised:
    No module named 'gdal'
    ```

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ autoapi_options = ['members', 'undoc-members', 'private-members']
 autodoc_mock_imports = ["numpy", "xarray", "fiona", "rasterio", "shapely",
                         "osgeo", "geopandas", "pandas", "statsmodels",
                         "affine", "rasterstats", "spotpy", "matplotlib",
-                        "scipy", "unidecode"]
+                        "scipy", "unidecode", "gdal"]
 
 # We are using mock imports in readthedocs, so probably safer to not run the notebooks
 nbsphinx_execute = 'never'
