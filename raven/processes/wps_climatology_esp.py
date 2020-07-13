80e793c419e6b4636a968ca7f6fce817c93fa63d
David Huard <huard.david@ouranos.ca>
2020-07-13 09:46:41 -0400

    change start and end date default handling. need testing

--- raven/processes/wps_climatology_esp.py
+++ raven/processes/wps_climatology_esp.py
@@ -18,8 +18,8 @@ import tempfile
 
 class ClimatologyEspProcess(Process):
     def __init__(self):
-        
-        
+
+
         fdate=LiteralInput('forecast_date', 'date of the forcast',
                                abstract='Date that the climatology-based ESP ensemble will be performed',
                                data_type='dateTime',
@@ -31,19 +31,19 @@ class ClimatologyEspProcess(Process):
                                default=30,
                                min_occurs=0,
                                max_occurs=1)
-        
+
         # The number of parameters will depend on the selected model in model_name
         params=LiteralInput('params', 'Comma separated list of model parameters',
                                abstract='Parameters to run the model',
                                data_type='string',
                                min_occurs=1,
                                max_occurs=1)
-                  
+
         inputs = [fdate, leadtime, params, wio.ts, wio.latitude, wio.longitude, wio.name,
               wio.model_name, wio.area, wio.elevation]
 
         outputs = [wio.forecast]
-    
+
         super(ClimatologyEspProcess, self).__init__(
             self._handler,
             identifier="climatology_esp",
@@ -63,30 +63,30 @@ class ClimatologyEspProcess(Process):
         kwds = {}
         for key, val in request.inputs.items():
             kwds[key] = request.inputs[key][0].data
-        
+
         # Get timeseries data that will be used to do the climatological ESP.
-     
+
         ts=request.inputs['ts'][0].file
         del kwds['ts']
-        
+
         # Get info from kwds but remove the ones that are not understood by RAVEN.
         # Have to delete these because or else I get an error: no config key named model_name
         forecast_date = kwds.pop('forecast_date')
         lead_time = kwds.pop('lead_time')
         model_name = kwds.pop('model_name')
-     
+
         # Get the model parameters, transform them to a list of floats and write them back to the kwds config.
         params=kwds['params']
         csv = params.replace('(', '').replace(')', '')
         params= list(map(float, csv.split(',')))
-        kwds['params']=params  
-          
+        kwds['params']=params
+
         qsims=forecasting.performClimatologyESP(model_name,ts,forecast_date,lead_time,**kwds)
-        
+
         # Prepare the forecast netcdf result file and send the path to the results output.
-        
-        forecastfile=(tempfile.mkdtemp() + '/forecast.nc')
+
+        forecastfile=(self.workdir + '/forecast.nc')
         qsims.to_netcdf(forecastfile)
         response.outputs['forecast'].file = forecastfile
-        
+
         return response
