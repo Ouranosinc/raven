2f2efd601f890d6a0b43d8e0540646b071f44da1
David Huard <huard.david@ouranos.ca>
2020-07-14 09:19:31 -0400

    pep8

--- raven/processes/wps_climatology_esp.py
+++ raven/processes/wps_climatology_esp.py
@@ -12,28 +12,46 @@ import tempfile
 class ClimatologyEspProcess(Process):
     def __init__(self):
 
-
-        fdate=LiteralInput('forecast_date', 'date of the forcast',
-                               abstract='Date that the climatology-based ESP ensemble will be performed',
-                               data_type='dateTime',
-                               min_occurs=1,
-                               max_occurs=1)
-        leadtime=LiteralInput('lead_time', 'Forecast lead-time',
-                               abstract='Duration of the forecast in days',
-                               data_type='integer',
-                               default=30,
-                               min_occurs=0,
-                               max_occurs=1)
+        fdate = LiteralInput(
+            "forecast_date",
+            "date of the forcast",
+            abstract="Date that the climatology-based ESP ensemble will be performed",
+            data_type="dateTime",
+            min_occurs=1,
+            max_occurs=1,
+        )
+        leadtime = LiteralInput(
+            "lead_time",
+            "Forecast lead-time",
+            abstract="Duration of the forecast in days",
+            data_type="integer",
+            default=30,
+            min_occurs=0,
+            max_occurs=1,
+        )
 
         # The number of parameters will depend on the selected model in model_name
-        params=LiteralInput('params', 'Comma separated list of model parameters',
-                               abstract='Parameters to run the model',
-                               data_type='string',
-                               min_occurs=1,
-                               max_occurs=1)
+        params = LiteralInput(
+            "params",
+            "Comma separated list of model parameters",
+            abstract="Parameters to run the model",
+            data_type="string",
+            min_occurs=1,
+            max_occurs=1,
+        )
 
-        inputs = [fdate, leadtime, params, wio.ts, wio.latitude, wio.longitude, wio.name,
-              wio.model_name, wio.area, wio.elevation]
+        inputs = [
+            fdate,
+            leadtime,
+            params,
+            wio.ts,
+            wio.latitude,
+            wio.longitude,
+            wio.name,
+            wio.model_name,
+            wio.area,
+            wio.elevation,
+        ]
 
         outputs = [wio.forecast]
 
@@ -48,7 +66,8 @@ class ClimatologyEspProcess(Process):
             outputs=outputs,
             keywords=[],
             status_supported=True,
-            store_supported=True)
+            store_supported=True,
+        )
 
     def _handler(self, request, response):
 
@@ -59,26 +78,28 @@ class ClimatologyEspProcess(Process):
 
         # Get timeseries data that will be used to do the climatological ESP.
 
-        ts = request.inputs['ts'][0].file
-        del kwds['ts']
+        ts = request.inputs["ts"][0].file
+        del kwds["ts"]
 
         # Get info from kwds but remove the ones that are not understood by RAVEN.
         # Have to delete these because or else I get an error: no config key named model_name
-        forecast_date = kwds.pop('forecast_date')
-        lead_time = kwds.pop('lead_time')
-        model_name = kwds.pop('model_name')
+        forecast_date = kwds.pop("forecast_date")
+        lead_time = kwds.pop("lead_time")
+        model_name = kwds.pop("model_name")
 
         # Get the model parameters, transform them to a list of floats and write them back to the kwds config.
-        params = kwds['params']
-        csv = params.replace('(', '').replace(')', '')
-        params = list(map(float, csv.split(',')))
-        kwds['params'] = params
+        params = kwds["params"]
+        csv = params.replace("(", "").replace(")", "")
+        params = list(map(float, csv.split(",")))
+        kwds["params"] = params
 
-        qsims = forecasting.perform_climatology_esp(model_name, ts, forecast_date, lead_time, **kwds)
+        qsims = forecasting.perform_climatology_esp(
+            model_name, ts, forecast_date, lead_time, **kwds
+        )
 
         # Prepare the forecast netcdf result file and send the path to the results output.
-        forecastfile = (self.workdir + '/forecast.nc')
+        forecastfile = self.workdir + "/forecast.nc"
         qsims.to_netcdf(forecastfile)
-        response.outputs['forecast'].file = forecastfile
+        response.outputs["forecast"].file = forecastfile
 
         return response
