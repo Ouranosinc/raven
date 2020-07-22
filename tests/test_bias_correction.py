73b29493f4ce3bfedbc22ef4e10f870ac1ebb5af
Long Vu <vu.long@ouranos.ca>
2020-07-22 16:08:11 -0400

    blacken test_bias_correction.py for pep8
    Running pep8 code style checks ...
    
    tests/test_bias_correction.py:8:1: W293 blank line contains whitespace
    
    tests/test_bias_correction.py:9:17: E225 missing whitespace around operator
    
    tests/test_bias_correction.py:9:121: E501 line too long (141 > 120 characters)
    
    tests/test_bias_correction.py:10:17: E225 missing whitespace around operator
    
    tests/test_bias_correction.py:10:121: E501 line too long (147 > 120 characters)
    
    tests/test_bias_correction.py:11:18: E225 missing whitespace around operator
    
    tests/test_bias_correction.py:12:1: W293 blank line contains whitespace
    
    tests/test_bias_correction.py:13:12: E225 missing whitespace around operator
    
    tests/test_bias_correction.py:14:12: E225 missing whitespace around operator
    
    tests/test_bias_correction.py:14:20: W291 trailing whitespace
    
    tests/test_bias_correction.py:15:1: W293 blank line contains whitespace
    
    tests/test_bias_correction.py:18:121: E501 line too long (143 > 120 characters)
    
    tests/test_bias_correction.py:19:73: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:19:121: E501 line too long (204 > 120 characters)
    
    tests/test_bias_correction.py:19:130: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:19:132: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:19:153: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:19:156: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:23:73: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:23:121: E501 line too long (204 > 120 characters)
    
    tests/test_bias_correction.py:23:130: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:23:132: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:23:153: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:23:156: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:24:73: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:24:121: E501 line too long (204 > 120 characters)
    
    tests/test_bias_correction.py:24:130: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:24:132: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:24:153: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:24:156: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:25:1: W293 blank line contains whitespace
    
    tests/test_bias_correction.py:27:1: W293 blank line contains whitespace
    
    tests/test_bias_correction.py:30:27: E231 missing whitespace after ','
    
    tests/test_bias_correction.py:31:9: F841 local variable 'Scen' is assigned to but never used
    
    tests/test_bias_correction.py:33:1: W293 blank line contains whitespace
    
    Makefile:246: recipe for target 'pep8' failed
    
    make: *** [pep8] Error 1

--- tests/test_bias_correction.py
+++ tests/test_bias_correction.py
@@ -5,30 +5,56 @@ import xclim.sdba as sdba
 
 class TestBiasCorrect:
     def test_bias_correction(self):
-    
-        ref_data='https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/dodsC/datasets/simulations/cmip5/atmos/day_MPI-ESM-LR_historical.ncml'
-        fut_data='https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/dodsC/datasets/simulations/cmip5/atmos/day_MPI-ESM-LR_historical+rcp85.ncml'
-        hist_data="https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/dodsC/datasets/gridded_obs/nrcan_v2.ncml"
-                  
-        lat=54.484
-        lon=-123.36   
-        
+
+        ref_data = "https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/dodsC/datasets/simulations/cmip5/atmos/day_MPI-ESM-LR_historical.ncml"
+        fut_data = "https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/dodsC/datasets/simulations/cmip5/atmos/day_MPI-ESM-LR_historical+rcp85.ncml"
+        hist_data = "https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/dodsC/datasets/gridded_obs/nrcan_v2.ncml"
+
+        lat = 54.484
+        lon = -123.36
+
         # CAREFUL! ERA5 IS NOT ThE SAME LONGITUDE
         # Also, latitude goes from high to low, so I need to have it go from lat+1 to lat-1 in the slice.
         # For the NRCan dataset, I cannot have more than about 10 years as I get a "NetCDF: DAP failure" which I think is related to a timeout.
-        ds = (xr.open_dataset(hist_data).sel(lat=slice(lat + 1, lat - 1),lon=slice(lon - 1, lon + 1), time=slice(dt.datetime(1991,1,1), dt.datetime(2010,12,31))).mean(dim={"lat", "lon"}, keep_attrs=True))
+        ds = (
+            xr.open_dataset(hist_data)
+            .sel(
+                lat=slice(lat + 1, lat - 1),
+                lon=slice(lon - 1, lon + 1),
+                time=slice(dt.datetime(1991, 1, 1), dt.datetime(2010, 12, 31)),
+            )
+            .mean(dim={"lat", "lon"}, keep_attrs=True)
+        )
 
         # For lon in 0-360 format, need to add an auto-checker.
         lon = 260
-        ds2 = (xr.open_dataset(ref_data).sel(lat=slice(lat - 1, lat + 1),lon=slice(lon - 1, lon + 1), time=slice(dt.datetime(1981,1,1), dt.datetime(2010,12,31))).mean(dim={"lat", "lon"}, keep_attrs=True))
-        ds3 = (xr.open_dataset(fut_data).sel(lat=slice(lat - 1, lat + 1),lon=slice(lon - 1, lon + 1), time=slice(dt.datetime(2041,1,1), dt.datetime(2070,12,31))).mean(dim={"lat", "lon"}, keep_attrs=True))
-     
+        ds2 = (
+            xr.open_dataset(ref_data)
+            .sel(
+                lat=slice(lat - 1, lat + 1),
+                lon=slice(lon - 1, lon + 1),
+                time=slice(dt.datetime(1981, 1, 1), dt.datetime(2010, 12, 31)),
+            )
+            .mean(dim={"lat", "lon"}, keep_attrs=True)
+        )
+        ds3 = (
+            xr.open_dataset(fut_data)
+            .sel(
+                lat=slice(lat - 1, lat + 1),
+                lon=slice(lon - 1, lon + 1),
+                time=slice(dt.datetime(2041, 1, 1), dt.datetime(2070, 12, 31)),
+            )
+            .mean(dim={"lat", "lon"}, keep_attrs=True)
+        )
+
         # Here data in ds, ds2 and ds3 are NaN!
-        
-        group_month_nowindow = sdba.utils.Grouper('time.month')
-        Adj = sdba.DetrendedQuantileMapping(nquantiles=50, kind='+', group=group_month_nowindow)
-        Adj.train(ds['pr'],ds2['pr'])
-        Scen = Adj.adjust(ds3['pr'], interp="linear")
+
+        group_month_nowindow = sdba.utils.Grouper("time.month")
+        Adj = sdba.DetrendedQuantileMapping(
+            nquantiles=50, kind="+", group=group_month_nowindow
+        )
+        Adj.train(ds["pr"], ds2["pr"])
+        Scen = Adj.adjust(ds3["pr"], interp="linear")
         Adj.ds.af  # adjustment factors.
-        
+
         print(Adj.ds.af)
