baba22fe182ca7757ae75f746d1529ff69a22586
Long Vu <vu.long@ouranos.ca>
2020-07-29 14:51:54 -0400

    final cruft refresh cookiecutter to get backports from other birds

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,14 +58,19 @@ autoapi_file_pattern = '*.py'
 autoapi_options = ['members', 'undoc-members', 'private-members']
 
 # To avoid having to install these and burst memory limit on ReadTheDocs.
+# List of all tested working mock imports from all birds so new birds can
+# inherit without having to test which work which do not.
 autodoc_mock_imports = ["numpy", "xarray", "fiona", "rasterio", "shapely",
                         "osgeo", "geopandas", "pandas", "statsmodels",
                         "affine", "rasterstats", "spotpy", "matplotlib",
                         "scipy", "unidecode", "gdal", "sentry_sdk", "dask",
                         "numba", "parse", "siphon", "sklearn", "cftime",
-                        "netCDF4", "bottleneck"]
+                        "netCDF4", "bottleneck", "ocgis", "geotiff", "geos",
+                        "hdf4", "hdf5", "zlib", "pyproj", "proj", "cartopy",
+                        "scikit-learn", "cairo"]
 
 # Monkeypatch constant because the following are mock imports.
+# Only works if numpy is actually installed and at the same time being mocked.
 import numpy
 numpy.pi = 3.1416
 
