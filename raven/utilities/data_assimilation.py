0bcef639c895b74a7a1557048056886d2fb53f35
richardarsenault <richard.arsenault@etsmtl.ca>
2020-11-02 22:07:28 -0500

    WIP data assimilation

--- raven/utilities/data_assimilation.py
+++ raven/utilities/data_assimilation.py
@@ -23,37 +23,41 @@ import numpy as np
 import tempfile
 import datetime as dt
 from pathlib import Path
-from raven.models import (
-    Raven,
-    GR4JCN,
-    HMETS,
-    MOHYSE,
-    HBVEC,
-)
-from raven.models.state import HRUStateVariables
+
+import pdb
+
+
 
 def assimilateQobsSingleDay(model,rvc,ts,days,number_members=25,precip_std=0.30,temp_std=2.0,qobs_std=0.15):
     tmp = Path(tempfile.mkdtemp())
     # Extract the Qobs, precip and temperature data for the day of interest
     nc_inputs = xr.open_dataset(ts)
-    pr=nc_inputs['pr'].sel(time=slice(days[0],days[1]))
-    tasmax=nc_inputs['tasmax'].sel(time=slice(days[0],days[1]))
-    tasmin=nc_inputs['tasmin'].sel(time=slice(days[0],days[1]))
-    qobs=nc_inputs['qobs'].sel(time=slice(days[0],days[1]))
+    
+    nc_inputs=nc_inputs.sel(time=days)
+    #pr=nc_inputs['pr'].sel(time=slice(days[0],days[1]))
+    #tasmax=nc_inputs['tasmax'].sel(time=slice(days[0],days[1]))
+    #tasmin=nc_inputs['tasmin'].sel(time=slice(days[0],days[1]))
+    #qobs=nc_inputs['qobs'].sel(time=slice(days[0],days[1]))
     
     # Check to see if all data is OK for assimilation (i.e. is Qobs=Nan or something like that, then skip)
-    if any(math.isnan(pr)) or any(math.isnan(tasmax)) or any(math.isnan(tasmin)) or any(math.isnan(qobs)):
-        do_assimilation=False # We will simply run the model with the current rvcs for the given time-step.
-    else: 
-        do_assimilation=True
-        
+    do_assimilation=True
+    for varname, da in nc_inputs.data_vars.items():
+        if np.isnan(da.data):
+            do_assimilation=False # We will simply run the model with the current rvcs for the given time-step.
+    
+    # Extract vars individually  
+    qobs=nc_inputs['qobs'].data   
+    pr=nc_inputs['rain'].data+nc_inputs['snow'].data
+    tasmax=nc_inputs['tmax'].data
+    tasmin=nc_inputs['tmin'].data    
     
     # Apply sampling input data uncertainty by perturbation (number_members, precip_std, temp_std, qobs_std)
-    pr_pert,tasmax_pert,tasmin_pert,qobs_pert,qobs_error=applyHydrometPerturbations(pr,tasmax,tasmin,qobs,number_members, precip_std, temp_std, qobs_std)
+    pr_pert,tasmax_pert,tasmin_pert,qobs_pert,qobs_error=applyHydrometPerturbations(pr,tasmin,tasmax,qobs,number_members, precip_std, temp_std, qobs_std)
     
     # Run the model number_member times, writing a NetCDF each time with adjusted precip/temp
     x_matrix=[]
     qsim_matrix=[]
+    pdb.set_trace()
     for i in range(0,number_members):
         x, qsim = runModelwithShortMeteo(model,rvc,pr,tasmax,tasmin,days,tmp)
         x_matrix.append(x)
@@ -61,19 +65,21 @@ def assimilateQobsSingleDay(model,rvc,ts,days,number_members=25,precip_std=0.30,
     
     # Apply the actual EnKF on the states to generate new, better states.
     if do_assimilation:
-        Xa=applyAssimilationOnStates(x,qobs_pert,qobs_error,qsim_matrix)
+        xa=applyAssimilationOnStates(x,qobs_pert,qobs_error,qsim_matrix)
     else:
-        Xa=x
+        xa=x
     
-    return Xa    
+    return xa    
     
     # Set new state variables in the rvc file
     # Updated RVC file after assimilation, ready for next simulation day.    
     
 def applyHydrometPerturbations(pr,tasmax,tasmin,qobs,number_members, precip_std, temp_std, qobs_std):
     
+    
+    
     # Temperature: Sample from normal distribution
-    random_noise_temperature = np.random.normal(0, temp_std, [tasmax.shape[0],number_members])
+    random_noise_temperature = np.random.normal(0, temp_std, [1,number_members])
     tasmax_pert=tasmax+random_noise_temperature
     tasmin_pert=tasmin+random_noise_temperature
     
@@ -84,7 +90,7 @@ def applyHydrometPerturbations(pr,tasmax,tasmin,qobs,number_members, precip_std,
     # Precipitation: Use a gamma function. Use shape and scale parameters independently
     shape_k=(pr**2)/(precip_std*pr)**2
     scale_t=((precip_std*pr)**2) / pr
-    pr_pert=np.random.gamma(shape=np.tile(shape_k,(1,number_members)),scale=np.time(scale_t,(1,number_members)))
+    pr_pert=np.random.gamma(shape=np.tile(shape_k,(1,number_members)),scale=np.tile(scale_t,(1,number_members)))
     
     # return the data. All values should now have size nDays x number_members
     return pr_pert,tasmax_pert,tasmin_pert,qobs_pert,qobs_error
@@ -118,6 +124,9 @@ def applyAssimilationOnStates(X,qobs_pert,qobs_error,qsim):
 
 def runModelwithShortMeteo(model,rvc,pr,tasmax,tasmin,days,tmp):
 
+    
+    pdb.set_trace()
+    
     # Convert the data arrays to datasets
     pr = pr.to_dataset(name='pr')
     tasmax = tasmax.to_dataset(name='tasmax')
@@ -153,20 +162,4 @@ def runModelwithShortMeteo(model,rvc,pr,tasmax,tasmin,days,tmp):
     qsim=model.q_sim
     
     return x, qsim
-    
-if __name__ == "__main__" :
-    
-    model = GR4JCN(tempfile.mkdtemp())
 
-    model.rvi.run_name = "test"
-
-    model.rvh.name = "Salmon"
-    model.rvh.area = "4250.6"
-    model.rvh.elevation = "843.0"
-    model.rvh.latitude = 54.4848
-    model.rvh.longitude = -123.3659
-
-    model.rvp.params = model.params(0.529, -3.396, 407.29, 1.072, 16.9, 0.947)
-    model.rvc.
-    
-    
