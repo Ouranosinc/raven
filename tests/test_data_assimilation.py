f57742c96464d1eb7cfef93699df2cfe1e0e4c01
richardarsenault <richard.arsenault@etsmtl.ca>
2020-11-07 11:31:13 -0500

    code for rvc parameter passing fix

--- tests/test_data_assimilation.py
+++ tests/test_data_assimilation.py
@@ -7,18 +7,20 @@ import pytest
 from raven.utilities.data_assimilation import assimilateQobsSingleDay
 from raven.models import GR4JCN
 from .common import TESTDATA
+import pdb
 
 class TestAssimilationGR4JCN:
     def test_simple(self):
         ts = TESTDATA["raven-gr4j-cemaneige-nc-ts"]
-
+        number_members=25
+        
         model = GR4JCN(tempfile.mkdtemp())
         
         start_date=dt.datetime(2000,1,1)
         end_date=dt.datetime(2002,1,1)
         
         model.rvi.start_date=start_date
-        model.rvi.end_date=dt.datetime(2000,1,2)
+        model.rvi.end_date=dt.datetime(2000,1,7)
         model.rvi.run_name = "test"
 
         model.rvh.name = "Salmon"
@@ -31,6 +33,20 @@ class TestAssimilationGR4JCN:
         model([ts,])
         rvc = model.outputs["solution"]
         
+        
+        model.resume(rvc)
+        xa=[]
+        xa.append(model.storage["Soil Water[0]"])
+        xa.append(model.storage["Soil Water[1]"])
+        xa=np.array(xa)
+        xa=np.tile(xa[:,-1],(number_members,1)).transpose()
+        
+        # Do first run at 7 days
+        date_list = [start_date + dt.timedelta(days=x) for x in range(7)]
+        xa = assimilateQobsSingleDay(model,xa,ts,date_list,number_members=number_members,precip_std=0.30,temp_std=2.0,qobs_std=0.15)            
+            
         for i in range(100):
-            xa = assimilateQobsSingleDay(model,rvc,ts,np.array([start_date+dt.timedelta(days=1+i)]),number_members=25,precip_std=0.30,temp_std=2.0,qobs_std=0.15)
-            #TODO : NEED TO UPDATE rvc FILE WITH xa
+            # TODO: Fix this once assimilation works
+            [xa,q_assim,q_openloop] = assimilateQobsSingleDay(model,xa,ts,np.array([start_date+dt.timedelta(days=1+i)]),number_members=number_members,precip_std=0.30,temp_std=2.0,qobs_std=0.15)            
+            
+            
