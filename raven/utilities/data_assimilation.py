7c977847941d87a8832f26c546116828a8f6f21b
David Huard <huard.david@ouranos.ca>
2020-11-10 16:23:59 -0500

    added perturbation function and test

--- raven/utilities/data_assimilation.py
+++ raven/utilities/data_assimilation.py
@@ -33,28 +33,28 @@ import pdb
 def assimilateQobsSingleDay(model,xa,ts,days,number_members=25,precip_std=0.30,temp_std=2.0,qobs_std=0.15):
     model=deepcopy(model)
     tmp = Path(tempfile.mkdtemp())
-    
+
     # Extract the Qobs, precip and temperature data for the day of interest
     nc_inputs = xr.open_dataset(ts)
     nc_inputs=nc_inputs.sel(time=days)
 
-    
+
     # Check to see if all data is OK for assimilation (i.e. is Qobs=Nan or something like that, then skip)
     do_assimilation=True
-   
+
     for varname, da in nc_inputs.data_vars.items():
         if np.isnan(da.data).any():
             do_assimilation=False # We will simply run the model with the current rvcs for the given time-step.
-    
-    # Extract vars individually  
-    qobs=np.array([nc_inputs['qobs'].data])   
+
+    # Extract vars individually
+    qobs=np.array([nc_inputs['qobs'].data])
     pr=np.array([nc_inputs['rain'].data+nc_inputs['snow'].data])
     tasmax=np.array([nc_inputs['tmax'].data])
-    tasmin=np.array([nc_inputs['tmin'].data])  
-    
+    tasmin=np.array([nc_inputs['tmin'].data])
+
     # Apply sampling input data uncertainty by perturbation (number_members, precip_std, temp_std, qobs_std)
     pr_pert,tasmax_pert,tasmin_pert,qobs_pert,qobs_error=applyHydrometPerturbations(pr,tasmax,tasmin,qobs[0,-1],number_members, precip_std, temp_std, qobs_std)
-    
+
     # Run the model number_member times, writing a NetCDF each time with adjusted precip/temp
     x_matrix=[]
     qsim_matrix=[]
@@ -63,35 +63,53 @@ def assimilateQobsSingleDay(model,xa,ts,days,number_members=25,precip_std=0.30,t
     #pdb.set_trace()
     for i in range(0,number_members):
         model.resume(rvc_main)
-        
-    
+
+
         model.rvc.hru_state=model.rvc.hru_state._replace(soil0=xa[0,i])
         model.rvc.hru_state=model.rvc.hru_state._replace(soil1=xa[1,i])
- 
+
         x, qsim = runModelwithShortMeteo(model,pr_pert[:,i],tasmax_pert[:,i],tasmin_pert[:,i],days,tmp)
         x_matrix.append(x)
         qsim_matrix.append(qsim.data)
-    
+
     qsim_matrix=np.array(qsim_matrix)
-   
+
     qsim_matrix=qsim_matrix[:,:,0]
-    
-    
+
+
     x_matrix=np.array(x_matrix)
     x_matrix=x_matrix[:,:,-1].transpose()
-  
+
     # Apply the actual EnKF on the states to generate new, better states.
     if do_assimilation:
         xa=applyAssimilationOnStates(x_matrix,qobs_pert,qobs_error,qsim_matrix[:,-1])
     else:
         xa=x_matrix
 
-    return [xa,qsim_matrix,qobs,model]    
-    
+    return [xa,qsim_matrix,qobs,model]
+
     # Set new state variables in the rvc file
-    # Updated RVC file after assimilation, ready for next simulation day.    
-    
-def applyHydrometPerturbations(pr,tasmax,tasmin,qobs,number_members, precip_std, temp_std, qobs_std): 
+    # Updated RVC file after assimilation, ready for next simulation day.
+
+
+def perturbation(da, dist, std, **kwargs):
+    """Return perturbed time series."""
+    nt = len(da.time)
+    size = list(kwargs.values()) + [nt, ]
+    dims = list(kwargs.keys()) + ["time"]
+
+    if dist == "norm":
+        r = np.random.normal(0, std, size=size)
+        return da + xr.DataArray(r, dims=dims, coords={"time": da.time})
+
+    elif dist == "gamma":
+        shape = (da ** 2) / (std * da) ** 2
+        scale = ((std * da) ** 2) / da
+        r = np.nan_to_num(np.random.gamma(shape=shape, scale=scale, size=size), nan=0.0)
+        return xr.DataArray(r, dims=dims, coords={"time": da.time})
+
+
+def applyHydrometPerturbations(pr,tasmax,tasmin,qobs,number_members, precip_std, temp_std, qobs_std):
 
     # Temperature: Sample from normal distribution
     random_noise_temperature = np.random.normal(0, temp_std, (number_members,tasmax.shape[1]))
@@ -99,11 +117,11 @@ def applyHydrometPerturbations(pr,tasmax,tasmin,qobs,number_members, precip_std,
     tasmin_pert=np.tile(tasmin,(number_members,1))+random_noise_temperature
     tasmax_pert=tasmax_pert.transpose()
     tasmin_pert=tasmin_pert.transpose()
-    
+
     # Qobs: Sample from normal distribution
     qobs_pert=np.random.normal(loc=np.tile(qobs,(1,number_members)), scale=qobs_std*np.tile(qobs,(1,number_members)))
     qobs_error=np.tile(qobs,(1,number_members))-qobs_pert # might have to copy/cat/repmat qobs here.
-   
+
     # Precipitation: Use a gamma function. Use shape and scale parameters independently
     shape_k=(pr**2)/(precip_std*pr)**2
     scale_t=((precip_std*pr)**2) / pr
@@ -113,15 +131,15 @@ def applyHydrometPerturbations(pr,tasmax,tasmin,qobs,number_members, precip_std,
     pr_pert=np.nan_to_num(pr_pert,nan=0.0)
     # return the data. All values should now have size nDays x number_members
     return pr_pert,tasmax_pert,tasmin_pert,qobs_pert,qobs_error
-    
-    
+
+
 def applyAssimilationOnStates(X,qobs_pert,qobs_error,qsim):
-    ''' 
+    '''
     X = states, matrix of nStates x number_members
     qobs_pert = perturbed observed streamflows of size 1 x number_members
     qobs_error = amount of error added to qobs to get qobs_pert , size 1 x number_members
     qsim = vector of simulated streamflows, size 1 x number_members
-    
+
     Taken from http://ccm.ucdenver.edu/reports/rep231.pdf
     University of colorado report on the efficient implementation of EnKF
     Jan Mandel, 2006
@@ -130,7 +148,7 @@ def applyAssimilationOnStates(X,qobs_pert,qobs_error,qsim):
     number_members=qobs_pert.shape[1]
     z=np.dot(qsim,np.ones((number_members,1)))
     HA=(qsim)-(z*np.ones((1,number_members)))/number_members
-    Y=qobs_pert-qsim    
+    Y=qobs_pert-qsim
     Re=np.dot(qobs_error,qobs_error.transpose())/number_members # ensemble covariance calculated by equation 19 in: The Ensemble Kalman Filter: theoretical formulation and practical implementation, Evensen 2003, https://link.springer.com/article/10.1007%2Fs10236-003-0036-9
     P=Re+(np.dot(HA,HA.transpose()))/(number_members-1)
     M=np.dot(P**-1,Y)
@@ -138,13 +156,13 @@ def applyAssimilationOnStates(X,qobs_pert,qobs_error,qsim):
     A=X-np.dot((np.dot(X,np.ones((number_members,1)))),np.ones((1,number_members)))/number_members
     Xa=X+(np.dot(A,Z))/(number_members-1)
     Xa=np.maximum(Xa,0)
-   
+
     return Xa
 
 def runModelwithShortMeteo(model,pr,tasmax,tasmin,days,tmp):
     model=deepcopy(model)
     #pdb.set_trace()
-    
+
     ds = xr.Dataset({
     'pr': xr.DataArray(
                 data   = pr,
@@ -175,16 +193,16 @@ def runModelwithShortMeteo(model,pr,tasmax,tasmin,days,tmp):
                 )
             },
     )
-    
+
     # write the netcdf file
     tsfile=tmp / 'tmp_assim_met.nc'
     ds.to_netcdf(tsfile)
-    
+
     # Update the model for this run
     model.rvi.start_date=days[0]
     model.rvi.end_date=days[-1]
 
-       
+
     # run the model with the input files
     model(tsfile,overwrite=True)
     # DO TESTS HERE FOR MODEL UPDATE PROBLEM
@@ -194,6 +212,6 @@ def runModelwithShortMeteo(model,pr,tasmax,tasmin,days,tmp):
     x.append(model.storage["Soil Water[0]"].copy(deep=True))
     x.append(model.storage["Soil Water[1]"].copy(deep=True))
     qsim=model.q_sim.copy(deep=True)
-    
+
     return x, qsim
 
--- tests/test_data_assimilation.py
+++ tests/test_data_assimilation.py
@@ -2,24 +2,35 @@ import datetime as dt
 import os
 import tempfile
 import numpy as np
+import xarray as xr
 import pytest
 import matplotlib.pyplot as plt
 from copy import deepcopy
-from raven.utilities.data_assimilation import assimilateQobsSingleDay
+from raven.utilities.data_assimilation import assimilateQobsSingleDay, perturbation
 from raven.models import GR4JCN
 from .common import TESTDATA
 import pdb
+
+
+def test_perturbation():
+    ts = TESTDATA["raven-gr4j-cemaneige-nc-ts"]
+    ds = xr.open_dataset(ts)
+
+    da = ds.tmax.isel(time=slice(0, 10))
+    p_tmax = perturbation(da, "norm", .01, members=50)
+    np.testing.assert_allclose(p_tmax.mean("members"), da, rtol=.1)
+
 
 class TestAssimilationGR4JCN:
     def test_simple(self):
         ts = TESTDATA["raven-gr4j-cemaneige-nc-ts"]
         number_members=25
-       
+
         model = GR4JCN(tempfile.mkdtemp())
-        
+
         start_date=dt.datetime(2000,1,1)
         end_date=dt.datetime(2002,1,1)
-        
+
         model.rvi.start_date=start_date
         model.rvi.end_date=dt.datetime(2000,1,2)
         model.rvi.run_name = "test"
@@ -35,30 +46,27 @@ class TestAssimilationGR4JCN:
 
         model([ts,])
         rvc = model.outputs["solution"]
-        
+
         plt.plot(model.q_sim)
         plt.show()
-        
+
         model.resume(rvc)
-        xa=[]
-        xa.append(model.storage["Soil Water[0]"])
-        xa.append(model.storage["Soil Water[1]"])
-        xa=np.array(xa)
-        xa=np.tile(xa[:,-1],(number_members,1)).transpose()
-        
+        xa = [model.storage["Soil Water[0]"].isel(time=-1).values, model.storage["Soil Water[1]"].isel(time=-1).values]
+        xa = np.tile(xa, (number_members, 1)).T
+
         # Do first run at 7 days
         date_list = [start_date + dt.timedelta(days=x) for x in range(10)]
-      
-        [xa,q_assim,q_openloop,model] = assimilateQobsSingleDay(model,xa,ts,date_list,number_members=number_members,precip_std=0.30,temp_std=2.0,qobs_std=0.15)            
+
+        [xa,q_assim,q_openloop,model] = assimilateQobsSingleDay(model,xa,ts,date_list,number_members=number_members,precip_std=0.30,temp_std=2.0,qobs_std=0.15)
         model=deepcopy(model)
         for i in range(1,20):
             start_date=start_date+dt.timedelta(days=10)
             date_list = [start_date + dt.timedelta(x) for x in range(10)]
-            [xa,q_a,q_ol,model] = assimilateQobsSingleDay(model,xa,ts,date_list,number_members=number_members,precip_std=0.30,temp_std=2.0,qobs_std=0.15)            
+            [xa,q_a,q_ol,model] = assimilateQobsSingleDay(model,xa,ts,date_list,number_members=number_members,precip_std=0.30,temp_std=2.0,qobs_std=0.15)
             model=deepcopy(model)
             q_assim=np.concatenate((q_assim,q_a),1)
             q_openloop=np.concatenate((q_openloop,q_ol),1)
-    
+
         plt.plot(q_assim.transpose(),'r')
         plt.plot(q_openloop.transpose(),'b')
         plt.show()
