9d6e4fc090338f6cd35c4d9687e0a84b547b0158
David Huard <huard.david@ouranos.ca>
2020-12-14 13:20:46 -0500

    remove ets hard-coded path

--- tests/test_hindcasting.py
+++ tests/test_hindcasting.py
@@ -14,11 +14,11 @@ but this is a good proof of concept.
 
 class TestHindcasting:
     def test_hindcasting_GEPS(self, tmpdir):
-                
+
         # Set the hindcasting date
         date=dt.datetime(2018,6,1)
-       
-        # Prepare a RAVEN model run using historical data, GR4JCN in this case. 
+
+        # Prepare a RAVEN model run using historical data, GR4JCN in this case.
         # This is a dummy run to get initial states. In a real forecast situation,
         # this run would end on the day before the forecast, but process is the same.
         ts = TESTDATA["raven-gr4j-cemaneige-nc-ts"]
@@ -33,10 +33,10 @@ class TestHindcasting:
             longitude=-123.3659,
             params=(0.529, -3.396, 407.29, 1.072, 16.9, 0.947),
         )
-        
+
         # Extract the final states that will be used as the next initial states
-        rvc=model.outputs["solution"]       
-        
+        rvc=model.outputs["solution"]
+
         # Collect the hindcast data for the date, location and climate model.
         # Limited to GEPS for now, for a restricted period (2017-06-01 to 2018-08-31)
         with open(TESTDATA["watershed_vector"], "rb") as f:
@@ -45,23 +45,20 @@ class TestHindcasting:
                 fcst = forecasting.get_hindcast_day(
                     region_coll, date, climate_model="GEPS"
                 )
-        
+
         # write the forecast data to file
         ts = f"{tmpdir}/fcstfile.nc"
         fcst.to_netcdf(ts)
-        
-        # write the forecast data to file
-        fcst.to_netcdf('/home/ets/src/raventest/raven/tests/hindcastfile.nc')
-        
+
         # It is necessary to clean the model state because the input variables of the previous
         # model are not the same as the ones provided in the forecast model. therefore, if we
         # do not clean, the model will simply add the hindcast file to the list of available
         # data provided in the testdata above. Then the dates will not work, and the model errors.
         model = GR4JCN()
-        
+
         model.rvc.parse(rvc.read_text())
-        
-        # And run the model with the forecast data.        
+
+        # And run the model with the forecast data.
         model(ts=ts,
             nc_index=range(fcst.dims.get('member')),
             start_date=dt.datetime(2018, 6, 1),
@@ -71,12 +68,12 @@ class TestHindcasting:
             latitude=54.4848,
             longitude=-123.3659,
             params=(0.529, -3.396, 407.29, 1.072, 16.9, 0.947),
-            overwrite=True, 
-            pr={'linear_transform': (1000.0, 0.0),'time_shift': -.25, 'deaccumulate':True}, 
+            overwrite=True,
+            pr={'linear_transform': (1000.0, 0.0),'time_shift': -.25, 'deaccumulate':True},
             tas={'time_shift': -.25},
-            )  
-        
+            )
+
         # The model now has the forecast data generated and it has 10 days of forecasts.
-        assert len(model.q_sim.values)==10
+        assert len(model.q_sim.values) == 10
         # Also see if GEPS has 20 members produced.
-        assert model.q_sim.values.shape[1]==20
+        assert model.q_sim.values.shape[1] == 20
