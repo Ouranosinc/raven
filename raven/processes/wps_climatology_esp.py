90733f2eb31c83086f8184bbc2e426296db71359
David Huard <huard.david@ouranos.ca>
2021-03-09 11:49:02 -0500

    add support for nc_spec in climatology_esp. updated notebook

--- raven/processes/wps_climatology_esp.py
+++ raven/processes/wps_climatology_esp.py
@@ -1,101 +1,83 @@
 import logging
 
-from pywps import LiteralInput, Process
+from pywps import LiteralInput
+from raven.processes import RavenProcess
 from ravenpy.utilities import forecasting
-
+from ravenpy.models import HMETS, GR4JCN, HBVEC, MOHYSE
 from . import wpsio as wio
 
+fdate = LiteralInput(
+    "forecast_date",
+    "date of the forecast",
+    abstract="Date that the climatology-based ESP ensemble will be performed",
+    data_type="dateTime",
+    min_occurs=1,
+    max_occurs=1,
+)
 
-class ClimatologyEspProcess(Process):
-    def __init__(self):
+duration = LiteralInput(
+    "forecast_duration",
+    "Forecast duration",
+    abstract="Duration of the forecast in days",
+    data_type="integer",
+    default=30,
+    min_occurs=0,
+    max_occurs=1,
+)
 
-        fdate = LiteralInput(
-            "forecast_date",
-            "date of the forecast",
-            abstract="Date that the climatology-based ESP ensemble will be performed",
-            data_type="dateTime",
-            min_occurs=1,
-            max_occurs=1,
-        )
-        duration = LiteralInput(
-            "forecast_duration",
-            "Forecast duration",
-            abstract="Duration of the forecast in days",
-            data_type="integer",
-            default=30,
-            min_occurs=0,
-            max_occurs=1,
-        )
+params = LiteralInput(
+    "params",
+    "Comma separated list of model parameters",
+    abstract="Parameters to run the model",
+    data_type="string",
+    min_occurs=1,
+    max_occurs=1,
+)
 
-        # The number of parameters will depend on the selected model in model_name
-        params = LiteralInput(
-            "params",
-            "Comma separated list of model parameters",
-            abstract="Parameters to run the model",
-            data_type="string",
-            min_occurs=1,
-            max_occurs=1,
-        )
 
-        inputs = [
-            fdate,
-            duration,
-            params,
-            wio.ts,
-            wio.latitude,
-            wio.longitude,
-            wio.name,
-            wio.model_name,
-            wio.area,
-            wio.elevation,
-            wio.nc_spec,
-            wio.rvc,
-            wio.rain_snow_fraction
-        ]
+class ClimatologyEspProcess(RavenProcess):
+    identifier = "climatology_esp"
+    abstract = "Forecast based on observed climatology"
+    title = "Climatological Ensemble Streamflow Prediction"
+    version = "0.1"
 
-        outputs = [
-            wio.forecast,
-        ]
+    inputs = [fdate, duration, wio.ts, wio.model_name, params,
+              wio.name, wio.area, wio.latitude, wio.longitude, wio.elevation,
+              wio.nc_spec, wio.rvc, wio.rain_snow_fraction]
 
-        super(ClimatologyEspProcess, self).__init__(
-            self._handler,
-            identifier="climatology_esp",
-            title="",
-            version="1.0",
-            abstract="",
-            metadata=[],
-            inputs=inputs,
-            outputs=outputs,
-            keywords=[],
-            status_supported=True,
-            store_supported=True,
-        )
+    outputs = [wio.forecast]
+    tuple_inputs = {
+        "hmets": HMETS.params,
+        "gr4jcn": GR4JCN.params,
+        "hbvec": HBVEC.params,
+        "mohyse": MOHYSE.params,
+    }
+
+    def parse_tuple(self, obj, model_name):
+        csv = obj.data.replace("(", "").replace(")", "")
+        arr = map(float, csv.split(","))
+        return self.tuple_inputs[model_name.lower()](*arr)
 
     def _handler(self, request, response):
+        response.update_status("PyWPS process {} started.".format(self.identifier), 0)
 
-        # Collect info from call
-        kwds = {}
-        for key, val in request.inputs.items():
-            kwds[key] = request.inputs[key][0].data
+        # Extract params to skip default processing in `self.options`
+        params = request.inputs.pop("params")[0]
 
-        kwds["ts"] = request.inputs["ts"][0].file
-        if "rvc" in request.inputs:
-            kwds["rvc"]=request.inputs["rvc"][0].file
-        # Get info from kwds but remove the ones that are not understood by RAVEN.
-        # Have to delete these because or else I get an error: no config key named model_name
-        forecast_date = kwds.pop("forecast_date")
-        forecast_duration = kwds.pop("forecast_duration")
-        model_name = kwds.pop("model_name")
-        
-        # Get the model parameters, transform them to a list of floats and write them back to the kwds config.
-        params = kwds["params"]
-        csv = params.replace("(", "").replace(")", "")
-        params = list(map(float, csv.split(",")))
-        kwds["params"] = params
+        # Input data files.
+        ts = self.meteo(request)
 
-        qsims = forecasting.perform_climatology_esp(
-            model_name, forecast_date, forecast_duration, workdir=self.workdir, **kwds
-        )
+        # Initial state
+        rvc = self.get_config(request, ids=("rvc",))["rvc"]
+
+        # Model options
+        kwds = self.options(request)
+        kwds["params"] = self.parse_tuple(params, kwds["model_name"].lower())
+        kwds["rvc"] = rvc
+        kwds["workdir"] = self.workdir
+
+        # Make forecasts. Only support one input file for now.
+        qsims = forecasting.perform_climatology_esp(ts=ts[0], **kwds)
 
         # Prepare the forecast netcdf result file and send the path to the results output.
         forecastfile = self.workdir + "/forecast.nc"
