Ouranosinc/raven

commit f0f0909d670ba621c6664649ad416d100539bb26

update notebooks

Makefile
@@ -5,7 +5,7 @@ THIS_FILE := $(lastword $(MAKEFILE_LIST))
 APP_ROOT := $(abspath $(lastword $(MAKEFILE_LIST))/..)
 APP_NAME := raven-wps
 
-WPS_URL := http://localhost:9099
+WPS_URL := http://0.0.0.0:9099
 
 GDAL_VERSION := $(shell gdal-config --version)
 

docs/source/notebooks/Comparing_hindcasts_and_ESP_forecasts.ipynb
@@ -241,10 +241,10 @@
     {
      "data": {
       "text/plain": [
-       "[<matplotlib.lines.Line2D at 0x7f878c32baf0>,\n",
-       " <matplotlib.lines.Line2D at 0x7f878c34fcd0>,\n",
-       " <matplotlib.lines.Line2D at 0x7f878c34f280>,\n",
-       " <matplotlib.lines.Line2D at 0x7f878c34f1f0>]"
+       "[<matplotlib.lines.Line2D at 0x7f16d6e362e0>,\n",
+       " <matplotlib.lines.Line2D at 0x7f16d732f430>,\n",
+       " <matplotlib.lines.Line2D at 0x7f16d72c7fa0>,\n",
+       " <matplotlib.lines.Line2D at 0x7f16d72c7e20>]"
       ]
      },
      "execution_count": 7,

commit 5820fb5c870376bbfcdda3ad0c82cfa94d279a4b

Makefile: fix refresh target not replacing the output url properly
Bug introduced by commit f0f0909d670ba621c6664649ad416d100539bb26.

```diff
< WPS_URL := http://localhost:9099
> WPS_URL := http://0.0.0.0:9099
```

Before fix:
```
$ make docs/source/notebooks/Extract_geographical_watershed_properties.ipynb.refresh
Refreshing notebook docs/source/notebooks/Extract_geographical_watershed_properties.ipynb
make[1]: Entering directory '/zstore/repos/raven'
bash -c 'WPS_URL="http://0.0.0.0:9099" FINCH_WPS_URL="https://pavics.ouranos.ca/twitcher/ows/proxy/finch/wps" FLYINGPIGEON_WPS_URL="https://pavics.uranos.ca/twitcher/ows/proxy/flyingpigeon/wps" jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=240 --output "docs/source/notebooks/Extract_geographical_watershed_properties.ipynb" --output-dir . "docs/source/notebooks/Extract_geographical_watershed_properties.ipynb"; sed -i "s@http://0.0.0.0:9099/outputs/@https://pavics.ouranos.ca/wpsoutputs/@g" "docs/source/notebooks/Extract_geographical_watershed_properties.ipynb"'
[NbConvertApp] Converting notebook docs/source/notebooks/Extract_geographical_watershed_properties.ipynb to notebook
```

After fix:
```
$ make docs/source/notebooks/Extract_geographical_watershed_properties.ipynb.refresh
Refreshing notebook docs/source/notebooks/Extract_geographical_watershed_properties.ipynb
make[1]: Entering directory '/zstore/repos/raven'

Makefile
@@ -5,7 +5,11 @@ THIS_FILE := $(lastword $(MAKEFILE_LIST))
 APP_ROOT := $(abspath $(lastword $(MAKEFILE_LIST))/..)
 APP_NAME := raven-wps
 
-WPS_URL := http://0.0.0.0:9099
+WPS_PORT := 9099
+WPS_URL := http://0.0.0.0:$(WPS_PORT)
+
+# If WPS_URL is overridden, this should also be overridden to match.
+WPS_OUTPUT_URL := http://localhost:$(WPS_PORT)
 
 GDAL_VERSION := $(shell gdal-config --version)
 
@@ -244,7 +248,7 @@ refresh-notebooks:
 NB_REFRESH_FILE := ""
 .PHONY: refresh-notebooks-impl
 refresh-notebooks-impl:
-	bash -c 'WPS_URL="$(WPS_URL)" FINCH_WPS_URL="$(FINCH_WPS_URL)" FLYINGPIGEON_WPS_URL="$(FLYINGPIGEON_WPS_URL)" jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=240 --output "$(NB_REFRESH_FILE)" --output-dir . "$(NB_REFRESH_FILE)"; sed -i "s@$(WPS_URL)/outputs/@$(OUTPUT_URL)/@g" "$(NB_REFRESH_FILE)"'
+	bash -c 'WPS_URL="$(WPS_URL)" FINCH_WPS_URL="$(FINCH_WPS_URL)" FLYINGPIGEON_WPS_URL="$(FLYINGPIGEON_WPS_URL)" jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=240 --output "$(NB_REFRESH_FILE)" --output-dir . "$(NB_REFRESH_FILE)"; sed -i "s@$(WPS_OUTPUT_URL)/outputs/@$(OUTPUT_URL)/@g" "$(NB_REFRESH_FILE)"'
 
 .PHONY: test_pdb
 test_pdb: